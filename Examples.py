ad0927e60a08b9cb9eb44fb019a12a35927b7dcd
Edi Muskardin <edi.muskardin@silicon-austria.com>
2021-04-22 20:56:51 +0200

    update ONFSM paper example

--- Examples.py
+++ Examples.py
@@ -289,21 +289,19 @@ def onfsm_mealy_paper_example():
     :return: learned ONFSM
     """
 
-    from random import seed
     from aalpy.SULs import OnfsmSUL
     from aalpy.oracles import UnseenOutputRandomWordEqOracle, UnseenOutputRandomWalkEqOracle
     from aalpy.learning_algs import run_non_det_Lstar
     from aalpy.utils import get_benchmark_ONFSM
-    seed(3)
 
     onfsm = get_benchmark_ONFSM()
     alphabet = onfsm.get_input_alphabet()
 
     sul = OnfsmSUL(onfsm)
-    eq_oracle = UnseenOutputRandomWalkEqOracle(alphabet, sul, num_steps=5000, reset_prob=0.09, reset_after_cex=True)
-    eq_oracle = UnseenOutputRandomWordEqOracle(alphabet, sul, num_walks=500, min_walk_len=4, max_walk_len=10)
+    eq_oracle = UnseenOutputRandomWalkEqOracle(alphabet, sul, num_steps=5000, reset_prob=0.25, reset_after_cex=True)
+    eq_oracle = UnseenOutputRandomWordEqOracle(alphabet, sul, num_walks=500, min_walk_len=2, max_walk_len=5)
 
-    learned_onfsm = run_non_det_Lstar(alphabet, sul, eq_oracle, n_sampling=15, print_level=3)
+    learned_onfsm = run_non_det_Lstar(alphabet, sul, eq_oracle, n_sampling=50, print_level=3)
 
     return learned_onfsm
 
