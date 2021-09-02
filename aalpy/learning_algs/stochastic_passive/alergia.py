8281fa57c0d87f32a125ce094edd5fa1f8cbdfe4
Edi Muskardin <edi.muskardin@silicon-austria.com>
2021-09-02 14:44:34 +0200

    infinite loop when getting succs

--- aalpy/learning_algs/stochastic_passive/alergia.py
+++ aalpy/learning_algs/stochastic_passive/alergia.py
@@ -1,150 +1,6 @@
 from math import sqrt, log
 
 
-class FptaNode:
-
-    def __init__(self, output):
-        self.output = output
-        self.frequency = 0
-        self.children = dict()
-        self.prefix = []
-
-
-def create_fpta(data):
-    root_node = FptaNode(data[0][0])
-    for seq in data:
-        if seq[0] != root_node.output:
-            print('All strings should have the same initial output')
-            assert False
-        curr_node = root_node
-
-        for el in seq[1:]:
-            if el not in curr_node.children.keys():
-                node = FptaNode(el)
-                node.prefix = list(curr_node.prefix)
-                node.prefix.append(el)
-                curr_node.children[el] = node
-
-            curr_node = curr_node.children[el]
-            curr_node.frequency += 1
-
-    return root_node
-
-
-def HoeffdingCompatibility(a: FptaNode, b: FptaNode, eps):
-    if a.children.keys() != b.children.keys():
-        return False
-
-    n1 = sum([child.frequency for child in a.children.values()])
-    n2 = sum([child.frequency for child in b.children.values()])
-
-    if n1 > 0 and n2 > 0:
-        for o in a.children.keys():
-            if abs(a.children[o].frequency / n1 - b.children[o].frequency / n2) > \
-                    ((sqrt(1 / n1) + sqrt(1 / n2)) * sqrt(0.5 * log(2 / eps))):
-                return False
-    return True
-
-
-def compatibility_test(a: FptaNode, b: FptaNode, eps):
-    if a.output != b.output:
-        return False
-
-    if not a.children.values() or not b.children.values():
-        return True
-
-    if not HoeffdingCompatibility(a, b, eps):
-        return False
-
-    for el in a.children.keys():
-        if not compatibility_test(a.children[el], b.children[el], eps):
-            return False
-
-    return True
-
-
-def get_paths(t, paths=None, current_path=None):
-    if paths is None:
-        paths = []
-    if current_path is None:
-        current_path = []
-
-    if not t.children:
-        paths.append(current_path)
-    else:
-        for inp, child in t.children.items():
-            current_path.append(inp)
-            get_paths(child, paths, list(current_path))
-    return paths
-
-
-def merge(a, t, q_r, q_b):
-    q_r_a, q_r_b = a, t
-    for i in q_r.prefix:
-        q_r_a = q_r_a.children[i]
-    prefix_to_b = t
-    for i in q_b.prefix[:-1]:
-        prefix_to_b = q_r_b.children[i]
-
-    q_r_b = prefix_to_b.children[q_b.prefix[-1]]
-    prefix_to_b.children[q_b.prefix[-1]] = q_r_a
-
-    paths_to_update = get_paths(q_r_b)
-
-    for path in paths_to_update:
-        curr_node_r = q_r_a
-        curr_node_b = q_r_b
-        for i in path:
-            curr_node_r = q_r_a.children[i]
-            curr_node_b = q_r_b.children[i]
-            curr_node_r.frequency += curr_node_b.frequency
-
-
-def run_alergia(data, eps):
-    assert 0 < eps <= 2
-
-    # FPTA T is kept as a data representation from which relevant statistics are retrieved during the execution of
-    # the algorithm
-    t = create_fpta(data)
-    # The FPTA A is iteratively transformed by merging nodes that have passed a statistical compatibility test
-    a = create_fpta(data)
-
-    t, a = get_fptas()
-
-    red = {t}  # representative nodes and will be included in the final output model # TODO FIX
-    blue = set()  # scheduled for testing
-    for c in t.children.values():
-        blue.add(c)
-
-    while blue:
-        lex_min_blue = min(list(blue), key=lambda x: len(x.prefix))
-
-        red_sorted = sorted(list(red), key=lambda x: len(x.prefix))
-
-        merged = False
-
-        for q_r in red_sorted:
-            if compatibility_test(q_r, lex_min_blue, eps):
-                print('Compatible', q_r.prefix, lex_min_blue.prefix)
-                merge(a, t, q_r, lex_min_blue)
-                merged = True
-                # break?
-
-        if not merged:
-            red.add(lex_min_blue)
-
-        blue.clear()
-        for r in red:
-            for child in r.children.values():
-                if child.prefix not in [c.prefix for c in red]:
-                    blue.add(child)
-
-    print(len(red))
-    for r in red:
-        print(r.prefix)
-    return a
-
-
 def get_fptas():
     root = FptaNode('A')
     root.children['B'] = FptaNode('B')
@@ -215,9 +71,174 @@ def get_fptas():
     return root, root1
 
 
+class FptaNode:
+
+    def __init__(self, output):
+        self.output = output
+        self.frequency = 0
+        self.children = dict()
+        self.prefix = []
+
+    def succs(self):
+        queue  = list(self.children.values())
+        succs = []
+        while queue:
+            node = queue.pop()
+            succs.append(node)
+            for child in node.children.values():
+                queue.append(child)
+        return succs
+
+    def get_child(self, prefix):
+        child = None
+        for p in prefix:
+            child = self.children[p]
+        assert child
+        return child
+
+
+def HoeffdingCompatibility(a: FptaNode, b: FptaNode, eps):
+    if a.children.keys() != b.children.keys():
+        return False
+
+    n1 = sum([child.frequency for child in a.children.values()])
+    n2 = sum([child.frequency for child in b.children.values()])
+
+    if n1 > 0 and n2 > 0:
+        for o in a.children.keys():
+            if abs(a.children[o].frequency / n1 - b.children[o].frequency / n2) > \
+                    ((sqrt(1 / n1) + sqrt(1 / n2)) * sqrt(0.5 * log(2 / eps))):
+                return False
+    return True
+
+
+class Alergia:
+    def __init__(self, data, eps=0.05):
+        assert 0 < eps <= 2
+
+        # self.t, self.a = self.create_fpta(data)
+        self.t, self.a = get_fptas()
+        self.eps = eps
+
+    def create_fpta(self, data):
+        root_node = FptaNode(data[0][0])
+        for seq in data:
+            if seq[0] != root_node.output:
+                print('All strings should have the same initial output')
+                assert False
+            curr_node = root_node
+
+            for el in seq[1:]:
+                if el not in curr_node.children.keys():
+                    node = FptaNode(el)
+                    node.prefix = list(curr_node.prefix)
+                    node.prefix.append(el)
+                    curr_node.children[el] = node
+
+                curr_node = curr_node.children[el]
+                curr_node.frequency += 1
+
+        return root_node
+
+    def compatibility_test(self, a: FptaNode, b: FptaNode):
+        if a.output != b.output:
+            return False
+
+        if not a.children.values() or not b.children.values():
+            return True
+
+        if not HoeffdingCompatibility(a, b, self.eps):
+            return False
+
+        for el in a.children.keys():
+            if not self.compatibility_test(a.children[el], b.children[el]):
+                return False
+
+        return True
+
+    def merge(self, q_r, q_b):
+        t_q_b = self.get_blue_node(q_b)
+        prefix_leading_to_state = q_b.prefix[:-1]
+        to_update = q_r
+        for p in prefix_leading_to_state:
+            to_update = to_update.children[p]
+
+        to_update.children[q_b.prefix[-1]] = q_r
+
+        paths = [p.prefix for p in self.get_leaf_nodes(t_q_b)]
+        paths = [p[len(t_q_b.prefix):] for p in paths]
+
+        for path in paths:
+            state_in_a = q_r
+            state_in_t = t_q_b
+            for p in path:
+                state_in_t = state_in_t.children[p]
+                state_in_a = state_in_a.children[p]
+                state_in_a.frequency += state_in_t.frequency
+        print('MERGE ENDED')
+
+    def get_leaf_nodes(self, origin):
+        leaves = []
+
+        def _get_leaf_nodes(origin):
+            if origin is not None:
+                if not origin.children:
+                    leaves.append(origin)
+                for n in origin.children.values():
+                    _get_leaf_nodes(n)
+
+        _get_leaf_nodes(origin)
+        return leaves
+
+    def run(self):
+
+        red = {self.a}  # representative nodes and will be included in the final output model
+        blue = self.a.succs()  # scheduled for testing
+
+        while blue:
+            lex_min_blue = min(list(blue), key=lambda x: len(x.prefix))
+
+            red_sorted = sorted(list(red), key=lambda x: len(x.prefix))
+
+            merged = False
+
+            for q_r in red_sorted:
+                if self.compatibility_test(self.get_blue_node(q_r), self.get_blue_node(lex_min_blue)):
+                    print('Compatible', q_r.prefix, lex_min_blue.prefix)
+                    self.merge(q_r, lex_min_blue)
+                    merged = True
+                    # break?
+
+            if not merged:
+                red.add(lex_min_blue)
+
+            blue.clear()
+            all_succs = set()
+            for r in red:
+                all_succs.update(r.succs())
+            print('REEEEEEEEEEEEEEEEEEEEEEEEEEE')
+            blue = list({el for el in all_succs if el.prefix not in [r.prefix for r in red]})
+            print('BLUE CREATION ENDED')
+
+
+        print(len(red))
+        for r in red:
+            print(r.prefix)
+        return self.a
+
+    def get_blue_node(self, red_node):
+        blue = self.t
+        for p in red_node.prefix:
+            blue = blue.children[p]
+        return blue
+
+def run_Alergia(data, eps):
+    alergia = Alergia(data, eps)
+    return alergia.run()
+
+
 if __name__ == '__main__':
     data = [[1, 2, 3, 4], [1, 1, 3, 4], [1, 2, 3, 2]]
-    model = run_alergia(data, 0.5)
-
+    model = run_Alergia(data, 0.5)
 
     exit(1)
