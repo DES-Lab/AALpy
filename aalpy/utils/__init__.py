3f6b991939ab12aaca4edf1a0f23179316e4f8f7
Edi Muskardin <edi.muskardin@silicon-austria.com>
2021-12-14 19:53:44 +0100

    fix wrong function referance

--- aalpy/utils/__init__.py
+++ aalpy/utils/__init__.py
@@ -1,7 +1,7 @@
 from .AutomatonGenerators import generate_random_dfa, generate_random_mealy_machine, generate_random_moore_machine, generate_random_markov_chain
 from .AutomatonGenerators import generate_random_mdp, generate_random_ONFSM
 from .FileHandler import save_automaton_to_file, load_automaton_from_file, visualize_automaton
-from .ModelChecking import model_check_experiment, mdp_2_prism_format, model_check_properties, get_properties_file, get_correct_prop_values, lbt
+from .ModelChecking import model_check_experiment, mdp_2_prism_format, model_check_properties, get_properties_file, get_correct_prop_values, compare_automata
 from .HelperFunctions import smm_to_mdp_conversion
 from .BenchmarkSULs import *
 from .DataHandler import *
