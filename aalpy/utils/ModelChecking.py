73a45b1b0504cd880ea5a9d86b369f21f6e59c37
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-01-24 09:43:33 +0100

    fix circular import

--- aalpy/utils/ModelChecking.py
+++ aalpy/utils/ModelChecking.py
@@ -6,7 +6,6 @@ import aalpy.paths
 from aalpy.SULs import MealySUL, DfaSUL, MooreSUL
 from aalpy.automata import Mdp, StochasticMealyMachine, MealyMachine, Dfa, MooreMachine
 from aalpy.base import DeterministicAutomaton
-from aalpy.oracles import RandomWMethodEqOracle
 
 prism_prob_output_regex = re.compile("Result: (\d+\.\d+)")
 
@@ -244,6 +243,8 @@ def compare_automata(aut_1: DeterministicAutomaton, aut_2: DeterministicAutomato
         A list of input sequences that revel different behaviour on both automata. Counterexamples are sorted by length.
     """
     #
+    from aalpy.oracles import RandomWMethodEqOracle
+
     type_map = {MooreMachine: MooreSUL, Dfa: DfaSUL, MealyMachine: MealySUL}
     assert set(aut_1.get_input_alphabet()) == set(aut_2.get_input_alphabet())
 
