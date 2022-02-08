7056950e4c4111812ee7a5da0ce60dfeb37c2d7e
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-02-08 10:05:10 +0100

    add pomdp

--- aalpy/utils/BenchmarkSULs.py
+++ aalpy/utils/BenchmarkSULs.py
@@ -350,3 +350,30 @@ class DateValidator:
             return False
 
         return True
+
+
+def get_small_pomdp():
+    q0 = MdpState("q0", "init")
+    q1 = MdpState("q1", "beep")
+    q2 = MdpState("q2", "beep")
+    q3 = MdpState("q3", "coffee")
+    q4 = MdpState("q4", "tea")
+
+    q0.transitions['but'].append((q0, 1))
+    q0.transitions['coin'].append((q1, 0.9))
+    q0.transitions['coin'].append((q2, 0.1))
+
+    q1.transitions['coin'].append((q1, 1))
+    q1.transitions['but'].append((q3, 1))
+
+    q2.transitions['coin'].append((q2, 0.1))
+    q2.transitions['coin'].append((q1, 0.9))
+    q2.transitions['but'].append((q4, 1))
+
+    q3.transitions['coin'].append((q3, 1))
+    q3.transitions['but'].append((q3, 1))
+
+    q4.transitions['coin'].append((q4, 1))
+    q4.transitions['but'].append((q4, 1))
+
+    return Mdp(q0, [q0, q1, q2, q3, q4])
