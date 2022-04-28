af389784c3cdd7b48d443201d0fb537432a0ce9c
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-04-28 10:59:55 +0200

    add more imports to import test

--- tests/tests_imports.py
+++ tests/tests_imports.py
@@ -9,6 +9,13 @@ class ImportTest(unittest.TestCase):
             import aalpy.utils
             import aalpy.SULs
             import aalpy.learning_algs
+            import aalpy.base
+            import aalpy.base.Automaton
+            import aalpy.utils.HelperFunctions
+            import aalpy.utils.DataHandler
+            import aalpy.utils.AutomatonGenerators
+            import aalpy.utils.ModelChecking
+            import aalpy.utils.FileHandler
         except:
             assert False
         assert True
