bac374bd1ffad21fb6372007ce40a9e4f3c031db
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-05-10 19:56:28 +0200

    made some progress

--- aalpy/learning_algs/deterministic_passive/rpni.py
+++ aalpy/learning_algs/deterministic_passive/rpni.py
@@ -3,10 +3,12 @@ from bisect import insort
 from copy import deepcopy
 
 from aalpy.SULs import DfaSUL
+from aalpy.automata import DfaState, Dfa
 from aalpy.utils import get_Angluin_dfa
 
 
 class RpniNode:
+
     def __init__(self, output):
         self.output = output
         self.children = dict()
@@ -39,11 +41,35 @@ def createPTA(data):
     return root_node
 
 
+def visualize_pta(rootNode):
+    from pydot import Dot, Node, Edge
+    graph = Dot('fpta', graph_type='digraph')
+
+    graph.add_node(Node(str(rootNode.prefix), label=f'{rootNode.output}'))
+
+    queue = [rootNode]
+    visitied = set()
+    visitied.add(rootNode.prefix)
+    while queue:
+        curr = queue.pop(0)
+        for i, c in curr.children.items():
+            if c.prefix not in visitied:
+                graph.add_node(Node(str(c.prefix), label=f'{c.output}'))
+            graph.add_edge(Edge(str(curr.prefix), str(c.prefix), label=f'{i}'))
+            if c.prefix not in visitied:
+                queue.append(c)
+            visitied.add(c.prefix)
+
+    graph.add_node(Node('__start0', shape='none', label=''))
+    graph.add_edge(Edge('__start0', str(rootNode.prefix), label=''))
+
+    graph.write(path=f'pta.pdf', format='pdf')
+
+
 class RPNI:
     def __init__(self, data):
         self.data = data
         self.root_node = createPTA(data)
-        self.run_rpni()
 
     def run_rpni(self):
 
@@ -57,12 +83,11 @@ class RPNI:
             for red_state in red:
                 if self.compatible(self.merge(red_state, lex_min_blue, copy_nodes=True)):
                     self.merge(red_state, lex_min_blue)
-                    print(red_state.prefix, lex_min_blue.prefix)
+                    print('MERGING', red_state.prefix, lex_min_blue.prefix)
                     merged = True
-                    print('MERGING')
 
             if not merged:
-                print('ADDING TO RED')
+                print('ADDING TO RED', lex_min_blue.prefix)
                 insort(red, lex_min_blue)
 
             blue.clear()
@@ -70,53 +95,63 @@ class RPNI:
                 for c in r.children.values():
                     if c not in red:
                         blue.append(c)
-            print(len(blue))
 
         assert sorted(red, key=lambda x: len(x.prefix)) == red
-        return red
+        print(f'RPNI returned {len(red)} state automaton')
+        return self.to_automaton(red)
 
     def compatible(self, r):
-        for seq in self.data:
+        for sequance in self.data:
             curr_node = r
-            for i, o in seq:
+            for i, o in sequance:
                 if i not in curr_node.children.keys():
-                    return False  # TODO I DONT KNOW
+                    return False  # TODO I DON'T KNOW
                 curr_node = curr_node.children[i]
-                if curr_node.output != curr_node.output:
+                if curr_node.output != o:
                     return False
-        print('COMPATIBLE')
         return True
 
     def merge(self, r, lex_min_blue, copy_nodes=False):
-        to_update = self.root_node if not copy_nodes else self.root_node.copy()
-        red_node = to_update
+        root_node = self.root_node.copy() if copy_nodes else self.root_node
+        lex_min_blue = lex_min_blue.copy() if copy_nodes else lex_min_blue
+
+        red_node = root_node
         for p in r.prefix:
             red_node = red_node.children[p]
 
         b_prefix = lex_min_blue.prefix
+        to_update = root_node
         for p in b_prefix[:-1]:
             to_update = to_update.children[p]
 
         to_update.children[b_prefix[-1]] = red_node
-        print('FOLDING', red_node.prefix, lex_min_blue.prefix)
         self.fold(red_node, lex_min_blue)
 
-        return to_update
+        return root_node
 
     def fold(self, red_node, blue_node):
-        # if red_node.output != blue_node.output:
-        #     print('  Changing output ', red_node.prefix, blue_node.prefix,
-        #           f'from {red_node.output} to {blue_node.output}')
         red_node.output = blue_node.output
         for i in blue_node.children.keys():
             if i in red_node.children.keys():
-                # print('  FOLD REC: ', red_node.children[i].prefix, blue_node.children[i].prefix)
                 self.fold(red_node.children[i], blue_node.children[i])
             else:
-                red_node.children[i] = blue_node.children[i]
-                # print('  FOLD Add: ', red_node.prefix, blue_node.children[i].prefix)
+                red_node.children[i] = blue_node.children[i].copy()
 
-        # print('  ENDING FOLD', red_node.prefix, blue_node.prefix)
+    def to_automaton(self, red):
+        state, automaton = DfaState, Dfa
+
+        initial_state = None
+        prefix_state_map = {}
+        for i, r in enumerate(red):
+            prefix_state_map[r.prefix] = DfaState(state_id=f's{i}', is_accepting=r.output)
+            if i == 0:
+                initial_state = prefix_state_map[r.prefix]
+
+        for r in red:
+            for i, c in r.children.items():
+                prefix_state_map[r.prefix].transitions[i] = prefix_state_map[c.prefix]
+        return Dfa(initial_state, list(prefix_state_map.values()))
+
 
 if __name__ == '__main__':
     dfa = get_Angluin_dfa()
@@ -133,9 +168,11 @@ if __name__ == '__main__':
         dfa_sul.post()
         data.append(seq)
 
-    data = [[('a', False), ('a', False), ('a', True)],
-            [('a', False), ('a', False), ('b', False), ('a', True)],
-            [('b', False), ('b', False), ('a', True)],
-            [('b', False), ('b', False), ('a', True), ('b', False), ('a', True)]]
-
-    model = RPNI(data)
+    # data = [[('a', False), ('a', False), ('a', True)],
+    #         [('a', False), ('a', False), ('b', False), ('a', True)],
+    #         [('b', False), ('b', False), ('a', True)],
+    #         [('b', False), ('b', False), ('a', True), ('b', False), ('a', True)],
+    #         [('a', False,), ('b', False,), ('a', False)]]
+    # a,bb,aab,aba
+    model = RPNI(data).run_rpni()
+    print(model)
