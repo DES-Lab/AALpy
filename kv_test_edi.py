dc45f45d392703b855af89b5fb1681025edda523
Andrea Pferscher <andrea.pferscher@ist.tugraz.at>
2022-11-17 13:39:18 +0100

    just active L* in test to compare required interaction

--- kv_test_edi.py
+++ kv_test_edi.py
@@ -2,12 +2,13 @@ from random import seed
 
 from aalpy.SULs import DfaSUL
 from aalpy.learning_algs.deterministic.KV import run_KV
+from aalpy.learning_algs.deterministic.LStar import run_Lstar
 from aalpy.oracles import StatePrefixEqOracle
 from aalpy.utils import generate_random_deterministic_automata
 
 automata_sizes = [1000,]
-input_al_sizes = [2, 4, 6, 8, 10]
-num_repeats = 5
+input_al_sizes = [3,5,10]
+num_repeats = 1
 
 i = 0
 for automata_size in automata_sizes:
@@ -22,7 +23,12 @@ for automata_size in automata_sizes:
 
             sul = DfaSUL(model)
             eq_oracle = StatePrefixEqOracle(input_al, sul, walks_per_state=15, walk_len=10)
-
+            print("KV:")
             learned_model = run_KV(input_al, sul, eq_oracle, cex_processing='rs', print_level=2)
+
+            sul = DfaSUL(model)
+            eq_oracle = StatePrefixEqOracle(input_al, sul, walks_per_state=15, walk_len=10)
+            print("L*:")
+            learned_model = run_Lstar(input_al, sul, eq_oracle, automaton_type='dfa', cex_processing='rs', print_level=2)
             if len(learned_model.states) != automata_size:
                 print('Failing seed', i)
--- test.py
+++ test.py
@@ -7,17 +7,19 @@ from aalpy.utils import generate_random_deterministic_automata, get_Angluin_dfa
 
 from random import seed
 seed(1)
-#dfa = get_Angluin_dfa()
-dfa = generate_random_deterministic_automata('dfa', num_states=500, input_alphabet_size=2, output_alphabet_size=2,num_accepting_states=20)
+dfa = get_Angluin_dfa()
+#dfa = generate_random_deterministic_automata('dfa', num_states=500, input_alphabet_size=2, output_alphabet_size=2,num_accepting_states=20)
 input_al = dfa.get_input_alphabet()
 sul = DfaSUL(dfa)
 
 #eq_oracle = RandomWordEqOracle(input_al, sul, min_walk_len=10, max_walk_len=15)
 eq_oracle = RandomWMethodEqOracle(input_al, sul, walks_per_state=20, walk_len=20)
+print("KV:")
 learned_model = run_KV(input_al, sul, eq_oracle, cex_processing='rs', print_level=1)
 
 sul1 = DfaSUL(dfa)
 eq_oracle = StatePrefixEqOracle(input_al, sul, walks_per_state=20, walk_len=20)
+print("L*:")
 learned_model = run_Lstar(input_al, sul1, eq_oracle, automaton_type='dfa', cex_processing='rs',
                           suffix_closedness=True,
                           closedness_type='prefix',
