37f51b73eddc9e8d4c81df49adbed41685bb34a1
Andrea Pferscher <andrea.pferscher@ist.tugraz.at>
2022-11-22 09:34:42 +0100

    minor changes in edis tests

--- kv_test_edi.py
+++ kv_test_edi.py
@@ -1,6 +1,6 @@
 from random import seed
 
-from aalpy.SULs import DfaSUL
+from aalpy.SULs import DfaSUL, MealySUL
 from aalpy.learning_algs.deterministic.KV import run_KV
 from aalpy.learning_algs.deterministic.LStar import run_Lstar
 from aalpy.oracles import StatePrefixEqOracle
@@ -16,19 +16,19 @@ for automata_size in automata_sizes:
         for _ in range(num_repeats):
             i += 1
             seed(i)
-            model = generate_random_deterministic_automata('dfa', num_states=automata_size,
+            model = generate_random_deterministic_automata('mealy', num_states=automata_size,
                                                            input_alphabet_size=input_size,
                                                            output_alphabet_size=2)
             input_al = model.get_input_alphabet()
 
-            sul = DfaSUL(model)
+            sul = MealySUL(model)
             eq_oracle = StatePrefixEqOracle(input_al, sul, walks_per_state=15, walk_len=10)
             print("KV:")
-            learned_model = run_KV(input_al, sul, eq_oracle, cex_processing='rs', print_level=2)
+            learned_model = run_KV(input_al, sul, eq_oracle, automaton_type='mealy', cex_processing='rs', print_level=2)
 
-            sul = DfaSUL(model)
+            sul = MealySUL(model)
             eq_oracle = StatePrefixEqOracle(input_al, sul, walks_per_state=15, walk_len=10)
             print("L*:")
-            learned_model = run_Lstar(input_al, sul, eq_oracle, automaton_type='dfa', cex_processing='rs', print_level=2)
+            learned_model = run_Lstar(input_al, sul, eq_oracle, automaton_type='mealy', cex_processing='rs', print_level=2)
             if len(learned_model.states) != automata_size:
                 print('Failing seed', i)
