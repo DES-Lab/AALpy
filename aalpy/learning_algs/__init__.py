aaf9d41a3d5e5eebfba5259b8bb62786530cbc12
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-11-24 08:46:55 +0100

    add run KV to learning_algs package

--- aalpy/learning_algs/__init__.py
+++ aalpy/learning_algs/__init__.py
@@ -1,5 +1,6 @@
 # public API for running automata learning algorithms
 from .deterministic.LStar import run_Lstar
+from .deterministic.KV import run_KV
 from .non_deterministic.OnfsmLstar import run_non_det_Lstar
 from .non_deterministic.AbstractedOnfsmLstar import run_abstracted_ONFSM_Lstar
 from .stochastic.StochasticLStar import run_stochastic_Lstar
