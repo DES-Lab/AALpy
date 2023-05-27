e6141e37f995c554173bf90650ad17ff18529c2e
zwergziege <b.v.b@gmx.at>
2023-05-27 21:15:20 +0200

    externalized debug stuff in non-det passive

--- aalpy/learning_algs/non_deterministic_passive/GeneralizedStateMerging.py
+++ aalpy/learning_algs/non_deterministic_passive/GeneralizedStateMerging.py
@@ -1,20 +1,73 @@
+import copy
 from queue import Queue
 import time
 from typing import Dict, Optional, Tuple
 
 from aalpy.learning_algs.non_deterministic_passive.rpni_helper_functions import Node
 
+
+class DebugInfo:
+    def __init__(self, lvl):
+        self.lvl = lvl
+
+    @staticmethod
+    def level_required(lvl):
+        def decorator(fn):
+            from functools import wraps
+            @wraps(fn)
+            def wrapper(*args, **kw):
+                if args[0].lvl < lvl:
+                    return
+                fn(*args, **kw)
+            return wrapper
+        return decorator
+
 class GeneralizedStateMerging:
-    def __init__(self, data, print_info=True):
+    class DebugInfo(DebugInfo):
+        lvl_required = DebugInfo.level_required
+
+        def __init__(self, lvl, instance):
+            super().__init__(lvl)
+            if lvl < 1:
+                return
+            self.instance = instance
+            self.log = []
+            self.pta = None
+
+        @lvl_required(1)
+        def pta_construction_done(self, start_time):
+            print(f'PTA Construction Time: {round(time.time() - start_time, 2)}')
+            self.pta = copy.deepcopy(self.instance.root)
+            states = self.instance.root.get_all_nodes()
+            leafs = [state for state in states if len(state.transitions.keys()) == 0]
+            depth = [len(state.prefix) for state in leafs]
+            print(f'PTA has {len(states)} states leading to {len(leafs)} leafs')
+            print(f'min / avg / max depth : {min(depth)} / {sum(depth) / len(depth)} / {max(depth)}')
+
+        @lvl_required(1)
+        def log_promote(self, node : Node, red_states):
+            self.log.append(["promote", (node.prefix,)])
+            print(f'\rCurrent automaton size: {len(red_states)}', end="")
+
+        @lvl_required(1)
+        def log_merge(self, a : Node, b : Node):
+            self.log.append(["merge", (a.prefix, b.prefix)])
+
+        @lvl_required(1)
+        def learning_done(self, red_states, start_time):
+            print(f'\nRPNI-GSM Learning Time: {round(time.time() - start_time, 2)}')
+            print(f'RPNI-GSM Learned {len(red_states)} state automaton.')
+            self.instance.root.visualize("wee", self.instance.data, self.pta)
+            #self.pta.visualize("pta")
+
+    def __init__(self, data, debug_lvl=0):
         self.data = data
-        self.print_info = print_info
+        self.debug = GeneralizedStateMerging.DebugInfo(debug_lvl, self)
 
         pta_construction_start = time.time()
-        self.root : Node = Node.createPTA(data)
-        self.log = []
-
-        if self.print_info:
-            print(f'PTA Construction Time: {round(time.time() - pta_construction_start, 2)}')
+        self.root : Node = copy.deepcopy(data) if isinstance(data, Node) else Node.createPTA(data)
+        self.root.prune(20)
+        self.debug.pta_construction_done(pta_construction_start)
 
     def run_rpni(self):
         start_time = time.time()
@@ -33,12 +86,10 @@ class GeneralizedStateMerging:
                     break
 
             if partition is None:
-                self.log.append(["promote", (blue_state.prefix,)])
                 red_states.append(blue_state)
-                if self.print_info:
-                    print(f'\rCurrent automaton size: {len(red_states)}', end="")
+                self.debug.log_promote(blue_state, red_states)
             else:
-                self.log.append(["merge", (red_state.prefix, blue_state.prefix)])
+                self.debug.log_merge(red_state, blue_state)
 
                 # use the partition for merging
                 for node in partition.keys():
@@ -51,9 +102,7 @@ class GeneralizedStateMerging:
                     if c not in red_states:
                         blue_states.append(c)
 
-        if self.print_info:
-            print(f'\nRPNI-GSM Learning Time: {round(time.time() - start_time, 2)}')
-            print(f'RPNI-GSM Learned {len(red_states)} state automaton.')
+        self.debug.learning_done(red_states, start_time)
 
         return self.root.to_automaton()
 
@@ -82,7 +131,7 @@ class GeneralizedStateMerging:
             red, blue = q.get()
             partition = get_partition(red)
 
-            if not Node.compatible_outputs(partition, blue) or 0 < Node.nondeterministic_additions(partition, blue) :
+            if not Node.compatible_outputs(partition, blue) or (0 < Node.nondeterministic_additions(partition, blue) and 0 < Node.nondeterministic_additions(blue, partition)) :
                 return None
 
             partitions[blue] = partition
