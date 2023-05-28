553917cf548d8002bbb6c3653ddfcd2bb867c3ec
zwergziege <b.v.b@gmx.at>
2023-05-28 14:01:54 +0200

    Added an alteration of the GSM framework code for stochastic Mealy machines

---
+++ aalpy/learning_algs/stochastic_passive/GeneralizedStateMerging.py
@@ -0,0 +1,198 @@
+import copy
+from math import sqrt, log
+from queue import Queue
+import time
+from typing import Dict, Tuple, Literal, Callable
+
+from aalpy.learning_algs.stochastic_passive.rpni_helper_functions import Node
+
+
+Score = bool
+ScoreFunction = Callable[[Node,Node], Score]
+def hoeffding_compatibility(eps) -> ScoreFunction:
+    def similar(a: Node, b: Node):
+        for in_sym in filter(lambda x : x in a.transitions.keys(), b.transitions.keys()):
+            a_count, b_count = (x.transition_count[in_sym] for x in [a,b])
+            a_total, b_total = (sum(x.values()) for x in [a_count, b_count])
+            if a_total == 0 or b_total == 0:
+                continue
+            for out_sym in set(a.transitions[in_sym].keys()).union(b.transitions[in_sym].keys()):
+                if abs(a_count[out_sym] / a_total - b_count[out_sym] / b_total) > ((sqrt(1 / a_total) + sqrt(1 / b_total)) * sqrt(0.5 * log(2 / eps))):
+                    return False
+        return True
+    return similar
+
+
+class DebugInfo:
+    def __init__(self, lvl):
+        self.lvl = lvl
+
+    @staticmethod
+    def level_required(lvl):
+        def decorator(fn):
+            from functools import wraps
+            @wraps(fn)
+            def wrapper(*args, **kw):
+                if args[0].lvl < lvl:
+                    return
+                fn(*args, **kw)
+            return wrapper
+        return decorator
+
+OutputBehavior = Literal["moore", "mealy"]
+TransitionBehavior = Literal["deterministic", "nondeterministic", "probabilistic"]
+
+class GeneralizedStateMerging:
+    class DebugInfo(DebugInfo):
+        lvl_required = DebugInfo.level_required
+
+        def __init__(self, lvl, instance):
+            super().__init__(lvl)
+            if lvl < 1:
+                return
+            self.instance = instance
+            self.log = []
+            self.pta = None
+
+        @lvl_required(1)
+        def pta_construction_done(self, start_time):
+            print(f'PTA Construction Time: {round(time.time() - start_time, 2)}')
+            self.pta = copy.deepcopy(self.instance.root)
+            states = self.instance.root.get_all_nodes()
+            leafs = [state for state in states if len(state.transitions.keys()) == 0]
+            depth = [len(state.prefix) for state in leafs]
+            print(f'PTA has {len(states)} states leading to {len(leafs)} leafs')
+            print(f'min / avg / max depth : {min(depth)} / {sum(depth) / len(depth)} / {max(depth)}')
+
+        @lvl_required(1)
+        def log_promote(self, node : Node, red_states):
+            self.log.append(["promote", (node.prefix,)])
+            print(f'\rCurrent automaton size: {len(red_states)}', end="")
+
+        @lvl_required(1)
+        def log_merge(self, a : Node, b : Node):
+            self.log.append(["merge", (a.prefix, b.prefix)])
+
+        @lvl_required(1)
+        def learning_done(self, red_states, start_time):
+            print(f'\nRPNI-GSM Learning Time: {round(time.time() - start_time, 2)}')
+            print(f'RPNI-GSM Learned {len(red_states)} state automaton.')
+            self.instance.root.visualize("wee", self.instance.data, self.pta)
+            #self.pta.visualize("pta")
+
+    def __init__(self, data, output_behavior : OutputBehavior = "moore",
+                 transition_behavior : TransitionBehavior = "deterministic",
+                 local_score : ScoreFunction = None, update_count : bool = False, debug_lvl=0):
+        self.data = data
+        self.debug = GeneralizedStateMerging.DebugInfo(debug_lvl, self)
+        self.output_behavior : OutputBehavior = output_behavior
+        self.transition_behavior : TransitionBehavior = transition_behavior
+
+        if local_score is None:
+            if output_behavior == "deterministic" :
+                local_score = lambda x,y : True
+            else :
+                local_score = hoeffding_compatibility(0.5)
+        self.local_score : ScoreFunction = local_score
+        self.update_transition_count = update_count
+
+        pta_construction_start = time.time()
+        self.root: Node
+        if isinstance(data, Node):
+            self.root = copy.deepcopy(data)
+        elif output_behavior == "moore":
+            self.root = Node.createPTA([d[1:] for d in data], data[0][0])
+        else :
+            self.root = Node.createPTA(data)
+        self.debug.pta_construction_done(pta_construction_start)
+
+        if transition_behavior == "deterministic":
+            if not self.root.is_deterministic():
+                raise ValueError("required deterministic automaton but input data is nondeterministic")
+
+    def local_merge_score(self, a : Node, b : Node):
+        if self.output_behavior == "moore" and not Node.moore_compatible(a,b):
+            return False
+        if self.transition_behavior == "deterministic" and not Node.deterministic_compatible(a,b):
+            return False
+        return self.local_score(a,b)
+
+    def run(self):
+        start_time = time.time()
+
+        # sorted list of states already considered
+        red_states = [self.root]
+        # used to get the minimal non-red state
+        blue_states = list(child for _, child in self.root.transition_iterator())
+
+        while blue_states:
+            blue_state = min(list(blue_states), key=lambda x: len(x.prefix))
+            partition = None
+            for red_state in red_states:
+                score, partition = self._partition_from_merge(red_state, blue_state)
+                if score:
+                    break
+
+            if not score:
+                red_states.append(blue_state)
+                self.debug.log_promote(blue_state, red_states)
+            else:
+                self.debug.log_merge(red_state, blue_state)
+
+                # use the partition for merging
+                for node in partition.keys():
+                    block = partition[node]
+                    node.transitions = block.transitions
+                    if self.update_transition_count:
+                        node.transition_count = block.transition_count
+
+            blue_states.clear()
+            for r in red_states:
+                for _, c in r.transition_iterator():
+                    if c not in red_states:
+                        blue_states.append(c)
+
+        self.debug.learning_done(red_states, start_time)
+
+        return self.root.to_automaton()
+
+    def _partition_from_merge(self, red: Node, blue: Node) -> Tuple[bool,Dict[Node, Node]] :
+        """
+        Compatibility check based on partitions
+        """
+
+        partitions = dict()
+
+        def get_partition(node: Node) -> Node:
+            if node not in partitions:
+                p = node.shallow_copy()
+                partitions[node] = p
+            else:
+                p = partitions[node]
+            return p
+
+        node = get_partition(self.root.get_by_prefix(blue.prefix[:-1]))
+        node.transitions[blue.prefix[-1][0]][blue.prefix[-1][1]] = red
+
+        q = Queue[Tuple[Node,Node]]()
+        q.put((red, blue))
+
+        while not q.empty():
+            red, blue = q.get()
+            partition = get_partition(red)
+
+            if not self.local_score(partition, blue) :
+                return False, dict()
+
+            partitions[blue] = partition
+
+            for in_sym, blue_transitions in blue.transitions.items():
+                partition_transitions = partition.transitions[in_sym]
+                for out_sym, blue_child in blue_transitions.items():
+                    if out_sym in partition_transitions:
+                        q.put((partition_transitions[out_sym], blue_child))
+                    else:
+                        # blue_child is blue after merging if there is a red state in the partition
+                        partition_transitions[out_sym] = blue_child
+                    partition.transition_count[in_sym][out_sym] += blue.transition_count[in_sym][out_sym]
+        return True, partitions
---
+++ aalpy/learning_algs/stochastic_passive/rpni_helper_functions.py
@@ -0,0 +1,156 @@
+import copy
+from collections import defaultdict
+from queue import Queue
+from typing import Set, Dict, Any, List, Tuple
+import pydot
+
+from aalpy.automata import StochasticMealyMachine, StochasticMealyState
+
+TransitionID = Tuple[Any, Any]
+Prefix = List[TransitionID]
+
+class Node:
+    __slots__ = ['transitions', 'prefix', "transition_count"]
+
+    def __init__(self, prefix):
+        self.transitions = defaultdict[Any, Dict[Any,Node]](dict)
+        self.transition_count = defaultdict[Any, defaultdict[Any, int]](lambda : defaultdict(lambda : 0))
+        self.prefix : Prefix = prefix
+
+    def __lt__(self, other):
+        return len(self.prefix) < len(other.prefix)
+
+    def __le__(self, other):
+        return len(self.prefix) <= len(other.prefix)
+
+    def __eq__(self, other):
+        return self.prefix == other.prefix
+
+    def __hash__(self):
+        return id(self)  # TODO This is a hack
+
+    def count(self):
+        return sum((sum(items.values()) for items in self.transition_count.values()))
+
+    def transition_iterator(self):
+        for in_sym, transitions in self.transitions.items():
+            for out_sym, node in transitions.items():
+                yield (in_sym, out_sym), node
+
+    def shallow_copy(self) -> 'Node':
+        node = Node(self.prefix)
+        node.transitions = {in_sym : dict(t) for in_sym, t in self.transitions.items()}
+        node.transition_count = copy.deepcopy(self.transition_count)
+        return node
+
+    def get_by_prefix(self, seq : Prefix) -> 'Node':
+        node = self
+        for in_sym, out_sym in seq:
+            node = node.transitions[in_sym][out_sym]
+        return node
+
+    def get_all_nodes(self) -> Set['Node']:
+        qu = Queue[Node]()
+        qu.put(self)
+        nodes = set()
+        while not qu.empty():
+            state = qu.get()
+            nodes.add(state)
+            for _, child in state.transition_iterator():
+                if child not in nodes:
+                    qu.put(child)
+        return nodes
+
+    def to_automaton(self) -> StochasticMealyMachine:
+        #TODO fix
+        nodes = self.get_all_nodes()
+        nodes.remove(self)  # dunno whether order is preserved?
+        nodes = [self] + list(nodes)
+
+        state_map = dict()
+        for i, r in enumerate(nodes):
+            state = StochasticMealyState(f's{i}')
+            state_map[r] = state
+            state.prefix = r.prefix
+
+        initial_state = state_map[self]
+
+        for r in nodes:
+            for in_sym, transitions in r.transitions.items():
+                count = r.transition_count[in_sym]
+                total = sum(count)
+                for out_sym, new_state in transitions.items():
+                    transition = (state_map[new_state], out_sym, count[out_sym] / total)
+                    state_map[r].transitions[in_sym].append(transition)
+
+        return StochasticMealyMachine(initial_state, list(state_map.values()))
+
+    def visualize(self, path : str):
+        graph = pydot.Dot('fpta', graph_type='digraph')
+
+        graph.add_node(pydot.Node(str(self.prefix), label=f'{self.count()}'))
+
+        queue = Queue[Node]()
+        queue.put(self)
+        visited = set()
+        visited.add(self)
+        while queue:
+            curr = queue.get()
+            for in_sym, options in curr.transitions.items():
+                if 1 < len(options) :
+                    color = 'red'
+                else:
+                    color = 'black'
+
+                for out_sym, c in options.items():
+                    if c not in visited:
+                        graph.add_node(pydot.Node(str(c.prefix), label=f'{c.count()}'))
+                        queue.put(c)
+                        visited.add(c)
+                    graph.add_edge(pydot.Edge(str(curr.prefix), str(c.prefix), label=f'{in_sym} / {out_sym} [{curr.transition_count[in_sym][out_sym]}]', color=color, fontcolor=color))
+
+        graph.add_node(pydot.Node('__start0', shape='none', label=''))
+        graph.add_edge(pydot.Edge('__start0', str(self.prefix), label=''))
+
+        graph.write(path=path, format='pdf')
+
+    def add_data(self, data):
+        for seq in data:
+            curr_node : Node = self
+            for in_sym, out_sym in seq:
+                transitions = curr_node.transitions[in_sym]
+                if out_sym not in transitions[out_sym]:
+                    node = Node(curr_node.prefix + [(in_sym, out_sym)])
+                    transitions[out_sym] = node
+                else:
+                    node = transitions[out_sym]
+                node.transition_count[in_sym][out_sym] += 1
+                curr_node = node
+    @staticmethod
+    def createPTA(data, initial_output = None) :
+        root_node = Node([(None, initial_output)])
+        root_node.add_data(data)
+        return root_node
+
+    def is_locally_deterministic(self):
+        return all(len(item) == 1 for item in self.transitions.values())
+
+    def is_deterministic(self):
+        return all(node.is_locally_deterministic() for node in self.get_all_nodes())
+
+    def deterministic_compatible(self, other : 'Node'):
+        common_keys = (key for key in self.transitions.keys() if key in other.transitions.keys())
+        return all(list(self.transitions[key].keys()) == list(other.transitions[key].keys()) for key in common_keys)
+
+    def is_moore(self):
+        output_dict = dict()
+        for node in self.get_all_nodes():
+            for (in_sym, out_sym), child in node.transition_iterator():
+                if child in output_dict.keys() and output_dict[child] != out_sym:
+                    return False
+                else:
+                    output_dict[child] = out_sym
+        return True
+
+    def moore_compatible(self, other : 'Node'):
+        return self.prefix[-1][1] == other.prefix[-1][1]
