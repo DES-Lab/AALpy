f2f6db8f0775be45d3a53920cd18b0a5d8003115
Edi Muskardin <edi.muskardin@silicon-austria.com>
2023-10-13 14:37:03 +0200

    push todos

---
+++ aalpy/learning_algs/vpda/VpdaClassificationTree.py
@@ -0,0 +1,316 @@
+from collections import defaultdict
+
+from aalpy.automata import DfaState, Dfa, MealyState, MealyMachine, MooreState, MooreMachine
+from aalpy.base import SUL
+
+
+class CTNode:
+    __slots__ = ['parent', 'path_to_node']
+
+    def __init__(self, parent, path_to_node):
+        self.parent = parent
+        self.path_to_node = path_to_node
+
+    def is_leaf(self):
+        pass
+
+
+class CTInternalNode(CTNode):
+    __slots__ = ['context_pair', 'children']
+
+    def __init__(self, context_pair: tuple, parent, path_to_node):
+        super().__init__(parent, path_to_node)
+        self.context_pair = context_pair
+        self.children = defaultdict(None)  # {True: None, False: None}
+
+    def is_leaf(self):
+        return False
+
+
+class CTLeafNode(CTNode):
+    __slots__ = ['access_string']
+
+    def __init__(self, access_string: tuple, parent, path_to_node):
+        super().__init__(parent, path_to_node)
+        self.access_string = access_string
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} '{self.access_string}'"
+
+    @property
+    def output(self):
+        c, p = self, self.parent
+        while p.parent:
+            c = p
+            p = p.parent
+        for output, child in p.children.items():
+            if child == c:
+                return output
+        assert False
+
+    def is_leaf(self):
+        return True
+
+
+class VpdaClassificationTree:
+    # TODO replace all dist. strings with context pairs appropriately
+    def __init__(self, alphabet: list, sul: SUL, cex: tuple):
+        self.sul = sul
+        self.alphabet = alphabet
+
+        self.leaf_nodes = {}
+        self.query_cache = dict()
+
+        self.sifting_cache = {}
+
+        initial_output = sul.query(())[-1]
+        cex_output = sul.query(cex)[-1]
+
+        self.query_cache[()] = initial_output
+
+        self.root = CTInternalNode(context_pair=tuple([(), ()]), parent=None, path_to_node=None)
+
+        initial_output_node = CTLeafNode(access_string=tuple(), parent=self.root, path_to_node=initial_output)
+        cex_output_node = CTLeafNode(access_string=cex, parent=self.root, path_to_node=cex_output)
+
+        self.root.children[initial_output] = initial_output_node
+        self.root.children[cex_output] = cex_output_node
+
+        self.leaf_nodes[tuple()] = initial_output_node
+        self.leaf_nodes[cex] = cex_output_node
+
+    def _sift(self, word):
+        """
+        Sifting a word into the classification tree.
+        Starting at the root, at every inner node (a CTInternalNode),
+        we branch into the child, depending on the result of the
+        membership query (word * node.distinguishing_string). Repeated until a leaf
+        (a CTLeafNode) is reached, which is the result of the sifting.
+
+        Args:
+
+            word: the word to sift into the discrimination tree (a tuple of all letters)
+
+        Returns:
+
+            the CTLeafNode that is reached by the sifting operation.
+        """
+        for letter in word:
+            assert letter is None or letter in self.alphabet
+
+        if word in self.sifting_cache:
+            return self.sifting_cache[word]
+
+        node = self.root
+        while not node.is_leaf():
+
+            query = node.context_pair[0] + word + node.context_pair[1]
+
+            if query not in self.query_cache.keys():
+                mq_result = self.sul.query(query)
+
+                mq_result = mq_result[-1]
+                self.query_cache[query] = mq_result
+            else:
+                mq_result = self.query_cache[query]
+
+            if mq_result not in node.children.keys():
+                new_leaf = CTLeafNode(access_string=word, parent=node, path_to_node=mq_result)
+                self.leaf_nodes[word] = new_leaf
+                node.children[mq_result] = new_leaf
+
+            node = node.children[mq_result]
+
+        self.sifting_cache[word] = node
+        assert node.is_leaf()
+        return node
+
+    def gen_hypothesis(self):
+        # for each CTLeafNode of this CT,
+        # create a state in the hypothesis that is labeled by that
+        # node's access string. The start state is the empty word
+
+        # TODO take a look at kv how it is done
+
+        return None
+
+    def _least_common_ancestor(self, node_1_id, node_2_id):
+        """
+        Find the distinguishing string of the least common ancestor
+        of the leaf nodes node_1 and node_2. Both nodes have to exist.
+        Adapted from https://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
+
+        Args:
+
+            node_1_id: first leaf node's id
+            node_2_id: second leaf node's id
+
+        Returns:
+
+            the distinguishing string of the lca
+
+        """
+
+        def ancestor(parent, node):
+            for child in parent.children.values():
+                if child.is_leaf():
+                    if child.access_string == node:
+                        return True
+                else:
+                    next_ancestor = ancestor(child, node)
+                    if next_ancestor:
+                        return True
+            return False
+
+        def findLCA(n1_id, n2_id):
+            node = self.leaf_nodes[n1_id]
+            parent = node.parent
+            while parent:
+                if ancestor(parent, n2_id):
+                    return parent
+                if parent.parent:
+                    parent = parent.parent
+                else:
+                    return parent
+            return None
+
+        return findLCA(node_1_id, node_2_id).context_pair
+
+    def update(self, cex: tuple, hypothesis):
+        """
+        Updates the classification tree based on a counterexample.
+        - For each prefix cex[:i] of the counterexample, get
+              s_i      = self.sift(cex[:i])    and
+              s_star_i = id of the state with the access sequence cex[:i]
+                         in the hypothesis
+          and let j be the least i such that s_i != s_star_i.
+        - Replace the CTLeafNode labeled with the access string of the state
+          that is reached by the sequence cex[:j-1] in the hypothesis
+          with an CTInternalNode with two CTLeafNodes: one keeps the old
+          access string, and one gets the new access string cex[:j-1].
+          The internal node is labeled with the distinguishing string (cex[j-1],*d),
+          where d is the distinguishing string of the LCA of s_i and s_star_i.
+
+        Args:
+            cex: the counterexample used to update the tree
+            hypothesis: the former (wrong) hypothesis
+
+        """
+        j = d = None
+        for i in range(1, len(cex) + 1):
+            s_i = self._sift(cex[:i]).access_string
+            hypothesis.execute_sequence(hypothesis.initial_state, cex[:i])
+            s_star_i = hypothesis.current_state.prefix
+            if s_i != s_star_i:
+                j = i
+                d = self._least_common_ancestor(s_i, s_star_i)
+                break
+        if j is None and d is None:
+            j = len(cex)
+            d = []
+        assert j is not None and d is not None
+
+        hypothesis.execute_sequence(hypothesis.initial_state, cex[:j - 1] or tuple())
+
+        self._insert_new_leaf(discriminator=(cex[j - 1], *d),
+                              old_leaf_access_string=hypothesis.current_state.prefix,
+                              new_leaf_access_string=tuple(cex[:j - 1]) or tuple(),
+                              new_leaf_position=self.sul.query((*cex[:j - 1], *(cex[j - 1], *d)))[-1])
+
+    def update_rs(self, cex: tuple, hypothesis):
+        """
+        Updates the classification tree based on a counterexample,
+        using Rivest & Schapire's counterexample processing
+        - Replace the CTLeafNode labeled with the access string of the state
+          that is reached by the sequence cex[:j-1] in the hypothesis
+          with an CTInternalNode with two CTLeafNodes: one keeps the old
+          access string, and one gets the new access string cex[:j-1].
+          The internal node is labeled with the distinguishing string (cex[j-1],*d),
+          where d is the distinguishing string of the LCA of s_i and s_star_i.
+
+        Args:
+            cex: the counterexample used to update the tree
+            hypothesis: the former (wrong) hypothesis
+
+        """
+        from aalpy.learning_algs.deterministic.CounterExampleProcessing import rs_cex_processing
+        v = max(rs_cex_processing(self.sul, cex, hypothesis, suffix_closedness=True), key=len)
+        a = cex[len(cex) - len(v) - 1]
+        u = cex[:len(cex) - len(v) - 1]
+        assert (*u, a, *v) == cex
+
+        hypothesis.execute_sequence(hypothesis.initial_state, u)
+        u_state = hypothesis.current_state.prefix
+        hypothesis.step(a)
+        ua_state = hypothesis.current_state.prefix
+
+        new_leaf_position = not hypothesis.execute_sequence(hypothesis.initial_state, cex)[-1]
+
+        self._insert_new_leaf(discriminator=v,
+                              old_leaf_access_string=ua_state,
+                              new_leaf_access_string=(*u_state, a),
+                              new_leaf_position=new_leaf_position)
+
+    def _insert_new_leaf(self, discriminator, old_leaf_access_string, new_leaf_access_string, new_leaf_position):
+        """
+        Inserts a new leaf in the classification tree by:
+        - moving the leaf node specified by <old_leaf_access_string> down one level
+        - inserting an internal node  at the former position of the old node (i.e. as the parent of the old node)
+        - adding a new leaf node with <new_leaf_access_string> as child of the new internal node / sibling of the old node
+        Could also be thought of as 'splitting' the old node into two (one of which keeps the old access string and one
+        of which gets the new one) with <discriminator> as the distinguishing string between the two.
+
+        where one of the resulting nodes keeps the old
+        node's access string and the other gets new_leaf_access_string.
+        Args:
+            discriminator: The distinguishing string of the new internal node
+            old_leaf_access_string: The access string specifying the leaf node to be 'split' (or rather moved down)
+            new_leaf_access_string: The access string of the leaf node that will be created
+            new_leaf_position: The path from the new internal node to the new leaf node
+
+        Returns:
+
+        """
+        other_leaf_position = not new_leaf_position
+
+        old_leaf = self.leaf_nodes[old_leaf_access_string]
+
+        # create an internal node at the same position as the old leaf node
+        # TODO is this context pair??
+        discriminator_node = CTInternalNode(context_pair=discriminator,
+                                            parent=old_leaf.parent, path_to_node=old_leaf.path_to_node)
+
+        # create the new leaf node and add it as child of the internal node
+        new_leaf = CTLeafNode(access_string=new_leaf_access_string,
+                              parent=discriminator_node,
+                              path_to_node=new_leaf_position)
+        self.leaf_nodes[new_leaf_access_string] = new_leaf
+
+        # redirect the old nodes former parent to the internal node
+        old_leaf.parent.children[old_leaf.path_to_node] = discriminator_node
+
+        # add the internal node as parent of the old leaf
+        old_leaf.parent = discriminator_node
+        old_leaf.path_to_node = other_leaf_position
+
+        # set the two nodes as children of the internal node
+        discriminator_node.children[new_leaf_position] = new_leaf
+        discriminator_node.children[other_leaf_position] = old_leaf
+
+        # sifting cache update
+        sifting_cache_outdated = []
+        if old_leaf in self.sifting_cache.values():
+            for prefix, node in self.sifting_cache.items():
+                if old_leaf == node:
+                    sifting_cache_outdated.append(prefix)
+
+        for to_delete in sifting_cache_outdated:
+            del self.sifting_cache[to_delete]
+
+    def _query_and_update_cache(self, word):
+        if word in self.query_cache.keys():
+            output = self.query_cache[word]
+        else:
+            output = self.sul.query(word)[-1]
+            self.query_cache[word] = output
+        return output
---
+++ aalpy/learning_algs/vpda/VpdaKV.py
@@ -0,0 +1,145 @@
+import time
+
+from aalpy.automata import Dfa, DfaState, MealyState, MealyMachine, MooreState, MooreMachine
+from aalpy.base import Oracle, SUL
+from aalpy.utils.HelperFunctions import print_learning_info
+from .VpdaClassificationTree import VpdaClassificationTree
+from ..deterministic.CounterExampleProcessing import counterexample_successfully_processed
+from ...base.SUL import CacheSUL
+
+print_options = [0, 1, 2, 3]
+counterexample_processing_strategy = [None, 'rs']
+
+
+def run_KV_vpda(alphabet: list, sul: SUL, eq_oracle: Oracle, cex_processing='rs',
+                max_learning_rounds=None, cache_and_non_det_check=True, return_data=False, print_level=2):
+    """
+    Executes the KV algorithm.
+
+    Args:
+
+        alphabet: input alphabet
+
+        sul: system under learning
+
+        eq_oracle: equivalence oracle
+
+        cex_processing: None for no counterexample processing, or 'rs' for Rivest & Schapire counterexample processing
+
+        max_learning_rounds: number of learning rounds after which learning will terminate (Default value = None)
+
+        cache_and_non_det_check: Use caching and non-determinism checks (Default value = True)
+
+        return_data: if True, a map containing all information(runtime/#queries/#steps) will be returned
+            (Default value = False)
+
+        print_level: 0 - None, 1 - just results, 2 - current round and hypothesis size, 3 - educational/debug
+            (Default value = 2)
+
+
+    Returns:
+
+        automaton of type automaton_type (dict containing all information about learning if 'return_data' is True)
+
+    """
+
+    assert print_level in print_options
+    assert cex_processing in counterexample_processing_strategy
+
+    start_time = time.time()
+    eq_query_time = 0
+    learning_rounds = 0
+
+    if cache_and_non_det_check:
+        # Wrap the sul in the CacheSUL, so that all steps/queries are cached
+        sul = CacheSUL(sul)
+        eq_oracle.sul = sul
+
+    empty_string_mq = sul.query(tuple())[-1]
+
+    initial_state = DfaState(state_id='s0', is_accepting=empty_string_mq)
+
+    initial_state.prefix = tuple()
+
+    # TODO Create 1-SEVPA class
+    # When creating a hypothesis, infer call transition destinations based on (loc, call) pairs
+
+    # TODO Create initial hypothesis
+    # Maybe move initialization of classification tree here
+    # Add a new method to it called generate_initial_hypothesis()
+    # Either -> one state and then procedure is same like in default KV (add cex later)
+    # Discover a new state
+
+    hypothesis = None
+    # Perform an equivalence query on this automaton
+    eq_query_start = time.time()
+    cex = eq_oracle.find_cex(hypothesis)
+
+    eq_query_time += time.time() - eq_query_start
+    if cex is not None:
+        cex = tuple(cex)
+
+        # initialise the classification tree to have a root
+        # labeled with the empty word as the distinguishing string
+        # and two leaves labeled with access strings cex and empty word
+        classification_tree = VpdaClassificationTree(alphabet=alphabet, sul=sul, cex=cex)
+
+        while True:
+            learning_rounds += 1
+            if max_learning_rounds and learning_rounds - 1 == max_learning_rounds:
+                break
+
+            hypothesis = classification_tree.gen_hypothesis()
+
+            if print_level == 2:
+                print(f'\rHypothesis {learning_rounds}: {hypothesis.size} states.', end="")
+
+            if print_level == 3:
+                # would be nice to have an option to print classification tree
+                print(f'Hypothesis {learning_rounds}: {hypothesis.size} states.')
+
+            if counterexample_successfully_processed(sul, cex, hypothesis):
+                # Perform an equivalence query on this automaton
+                eq_query_start = time.time()
+                cex = eq_oracle.find_cex(hypothesis)
+                eq_query_time += time.time() - eq_query_start
+
+                if cex is None:
+                    break
+                else:
+                    cex = tuple(cex)
+
+                if print_level == 3:
+                    print('Counterexample', cex)
+
+            if cex_processing == 'rs':
+                classification_tree.update_rs(cex, hypothesis)
+            else:
+                classification_tree.update(cex, hypothesis)
+
+    total_time = round(time.time() - start_time, 2)
+    eq_query_time = round(eq_query_time, 2)
+    learning_time = round(total_time - eq_query_time, 2)
+
+    info = {
+        'learning_rounds': learning_rounds,
+        'automaton_size': hypothesis.size,
+        'queries_learning': sul.num_queries,
+        'steps_learning': sul.num_steps,
+        'queries_eq_oracle': eq_oracle.num_queries,
+        'steps_eq_oracle': eq_oracle.num_steps,
+        'learning_time': learning_time,
+        'eq_oracle_time': eq_query_time,
+        'total_time': total_time,
+        'cache_saved': sul.num_cached_queries,
+    }
+
+    if print_level > 0:
+        if print_level == 2:
+            print("")
+        print_learning_info(info)
+
+    if return_data:
+        return hypothesis, info
+
+    return hypothesis
