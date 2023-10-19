a65cbe7f38988a0fb639a33df245f955eeb35889
Edi Muskardin <edi.muskardin@silicon-austria.com>
2023-10-19 10:34:11 +0200

    seeems to work at least on 2-3 examples

--- test_edi.py
+++ test_edi.py
@@ -14,18 +14,17 @@ from aalpy.utils.BenchmarkSevpaModels import *
 #
 # print(sevpa.input_alphabet)
 
-model_under_learning = vpa_for_L15()
+for i, vpa in enumerate([vpa_for_L1(), vpa_for_L2(), vpa_for_L3(), vpa_for_L4(), vpa_for_L5(), vpa_for_L7(), vpa_for_L8(),
+            vpa_for_L9(), vpa_for_L10(), vpa_for_L11(), vpa_for_L12(),vpa_for_L13(), vpa_for_L14(), vpa_for_L15()]):
 
-merged_input_alphabet = SevpaAlphabet(list(model_under_learning.internal_set),
-                                      list(model_under_learning.call_set),
-                                      list(model_under_learning.return_set))
+    print(f'VPA {i + 1 if i < 6 else i + 2}')
+    model_under_learning = vpa
 
-sul = VpaSUL(model_under_learning, include_top=False, check_balance=False)
+    merged_input_alphabet = SevpaAlphabet(list(model_under_learning.internal_set),
+                                          list(model_under_learning.call_set),
+                                          list(model_under_learning.return_set))
 
-eq_oracle = RandomWordEqOracle(alphabet=merged_input_alphabet.get_merged_alphabet(), sul=sul, num_walks=10000)
-model = run_KV_vpda(alphabet=merged_input_alphabet, sul=sul, eq_oracle=eq_oracle, print_level=3, max_learning_rounds=3)
+    sul = VpaSUL(model_under_learning, include_top=False, check_balance=False)
 
-model_sul = SevpaSUL(model, include_top=True, check_balance=True)
-print(model_sul.query(('(', ')')))
-print(model_sul.query(('[', ')')))
-print(model_sul.query(('[', '(', ')', ']')))
+    eq_oracle = RandomWordEqOracle(alphabet=merged_input_alphabet.get_merged_alphabet(), sul=sul, num_walks=10000)
+    model = run_KV_vpda(alphabet=merged_input_alphabet, sul=sul, eq_oracle=eq_oracle, print_level=3, max_learning_rounds=3)
