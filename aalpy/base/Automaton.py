c8fd5dd2a36ad55f8d2ce802cadf94251035f964
zwergziege <b.v.b@gmx.at>
2023-11-07 11:54:06 +0100

    Removed unholy TODOs to satisfy my grateful master. May 3.11 never come!

--- aalpy/base/Automaton.py
+++ aalpy/base/Automaton.py
@@ -155,14 +155,14 @@ class Automaton(ABC, Generic[AutomatonStateType]):
 
     @staticmethod
     @abstractmethod
-    def from_state_setup(state_setup: dict) -> 'Automaton': # TODO -> Self on bump to 3.11
+    def from_state_setup(state_setup: dict) -> 'Automaton':
         pass
 
     @abstractmethod
     def to_state_setup(self):
         pass
 
-    def copy(self) -> 'Automaton': # TODO -> Self on bump to 3.11
+    def copy(self) -> 'Automaton':
         return self.from_state_setup(self.to_state_setup())
 
     def __reduce__(self):
