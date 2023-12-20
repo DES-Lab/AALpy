0f87a0d01595f6dfa5057889167f6764b66a44ca
Edi Muskardin <edi.muskardin@silicon-austria.com>
2023-12-20 15:56:04 +0100

    add top-level imports

---
+++ aalpy/__init__.py
@@ -0,0 +1,7 @@
+from aalpy.base import *
+from aalpy.automata import *
+from aalpy.oracles import *
+from aalpy.learning_algs import *
+from aalpy.SULs import *
+from aalpy.utils import *
+from aalpy.paths import *
