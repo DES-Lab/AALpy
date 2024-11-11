a4930fa73a78f72eb01f1357b83631a4253f32b2
Stefanos Anagnostou <steve.anunknown@gmail.com>
2024-11-11 18:55:29 +0200

    basic iteration over models to test that they are loaded correctly

--- Benchmarking/mealy_conformance_testing.py
+++ Benchmarking/mealy_conformance_testing.py
@@ -1,36 +1,23 @@
 import os
+import itertools
 from pathlib import Path
 
+from aalpy.utils.FileHandler import load_automaton_from_file
 from aalpy.learning_algs.deterministic import KV
 import aalpy.oracles
-from aalpy.utils.FileHandler import (
-    load_automaton_from_file,
-    save_automaton_to_file,
-    visualize_automaton,
-)
 
 root = os.getcwd() + "/DotModels"
-
 protocols = ["MQTT", "TCP", "TLS"]
-
 directories = [ Path(root + '/' + prot) for prot in protocols]
-
-# files = itertools.chain.from_iterable(dir.iterdir() for dir in directories)
-# 
-# models = (load_automaton_from_file(f, 'mealy') for f in files)
+files = itertools.chain.from_iterable(dir.iterdir() for dir in directories)
+models = iter(load_automaton_from_file(f, 'mealy') for f in files)
 
 equivalence_oracles = [
         aalpy.oracles.RandomWalkEqOracle,
         aalpy.oracles.RandomWordEqOracle
         ]
 
-for dir in directories:
-    for f in dir.iterdir():
-        print(f)
-        try:
-            model = load_automaton_from_file(f, 'mealy')
-        except ValueError:
-            print(f"Value error reading file {f}")
-            continue
-        alphabet = model.get_input_alphabet()
-        print(alphabet)
+for model in models:
+    print(model.get_input_alphabet())
+
+
