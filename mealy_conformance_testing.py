173a6244ba7bf301b9284dc7ce2ad6d27f23b90f
steve-anunknown <steve.anunknown@gmail.com>
2024-11-28 15:15:19 +0200

    remove intermediate_hypotheses

--- mealy_conformance_testing.py
+++ mealy_conformance_testing.py
@@ -3,91 +3,96 @@ import pandas as pd
 import pickle
 import random
 from pathlib import Path
-
 from aalpy import oracles
 from aalpy.SULs.AutomataSUL import AutomatonSUL
 from aalpy.learning_algs.deterministic.LStar import run_Lstar
 from aalpy.utils.FileHandler import load_automaton_from_file
 
 
-if not os.path.exists('results.pkl'):
+resultsdir = "results"
+
+attributes = ['learning_rounds',
+              'automaton_size',
+              'queries_learning',
+              'steps_learning',
+              'queries_eq_oracle',
+              'steps_eq_oracle',
+              'learning_time',
+              'eq_oracle_time',
+              'total_time',
+              'characterization_set',
+              'intermediate_hypotheses']
+
+if not os.path.exists(resultsdir):
+
+    if not os.path.exists(resultsdir):
+        os.makedirs(resultsdir)
 
     root = os.getcwd() + "/DotModels"
-    protocols = ["MQTT", "TCP", "TLS"]
+    protocols = ["MQTT", "TLS"]
     directories = [Path(root + '/' + prot) for prot in protocols]
     files = [file for dir in directories for file in dir.iterdir()]
     models = [load_automaton_from_file(f, 'mealy') for f in files]
-    
+
     assert len(files) == len(models)
-    
-    if not os.path.exists('intermediate_hypotheses'):
-        os.makedirs('intermediate_hypotheses')
-    
+
+
     class StatePrefixEqOracleTrue(oracles.StatePrefixEqOracle):
         def __init__(self, alphabet, sul):
             super().__init__(alphabet, sul, walks_per_state=50, depth_first=True)
-    
+
     class StatePrefixEqOracleFalse(oracles.StatePrefixEqOracle):
         def __init__(self, alphabet, sul):
             super().__init__(alphabet, sul, walks_per_state=50, depth_first=False)
-    
+
     results = {} 
     for file, model in zip(files, models):
         alphabet = model.get_input_alphabet()
         sul = AutomatonSUL(model)
-    
+
         oracle_status = []
-    
+
         # deterministic
         orcs = [
-                    StatePrefixEqOracleFalse(alphabet, sul),
-                    StatePrefixEqOracleTrue(alphabet, sul),
-                    oracles.BreadthFirstExplorationEqOracle(alphabet, sul),
-                    oracles.KWayStateCoverageEqOracle(alphabet, sul),
-                    oracles.TransitionFocusOracle(alphabet, sul, walk_len=40),
-                    oracles.CacheBasedEqOracle(alphabet, sul),
-                    oracles.PacOracle(alphabet, sul),
-                    oracles.RandomWMethodEqOracle(alphabet, sul, walks_per_state=50, walk_len=40)
-                    ]
-    
-        # eqo = RandomWordEqOracle(alphabet, sul, num_walks=5000, min_walk_len=10, max_walk_len=100)
+                StatePrefixEqOracleFalse(alphabet, sul),
+                StatePrefixEqOracleTrue(alphabet, sul),
+                oracles.KWayStateCoverageEqOracle(alphabet, sul, random_walk_len=40),
+                oracles.TransitionFocusOracle(alphabet, sul, walk_len=40),
+                oracles.CacheBasedEqOracle(alphabet, sul),
+                oracles.PacOracle(alphabet, sul),
+                oracles.RandomWMethodEqOracle(alphabet, sul, walks_per_state=50, walk_len=40)
+                ]
+
         correct = model.size
         states = []
         for eqo in orcs:
-    
+
             l_star_model, info = run_Lstar(alphabet, sul, eqo, 'mealy', print_level=0, return_data=True)
-            size = info['automaton_size']
-            steps = info['steps_learning']
-            queries = info['queries_learning']
-            intermediate = info['intermediate_hypotheses']
-            
             name = eqo.__class__.__name__
-            path = f"intermediate_hypotheses/{file.stem}/{name}"
+            infodir = resultsdir + '/' + name + '/' + file.stem
+            if not os.path.exists(infodir):
+                os.makedirs(infodir)
+
+            infofile = resultsdir + '/' + name + '/' + file.stem + '/info.pkl'
+            with open(infofile, 'wb') as f:
+                pickle.dump(info, f)
+                
+            intermediate = info['intermediate_hypotheses']
             for number, hyp in enumerate(intermediate):
-                # check if directory exists
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                hyp.save(file_path=(path + f"/h{number}.dot"))
-    
+                hyp.save(file_path=(infodir+ f"/h{number}.dot"))
+
             # also save the diffs of consecutive hypotheses
             # this can be done by executing diff on the command line
             # diff hyp1.dot hyp2.dot > diff1_2.txt
             for i in range(1, len(intermediate)):
-                os.system(f"diff {path}/h{i - 1}.dot {path}/h{i}.dot > {path}/diff_{i - 1}_{i}.txt")
-           
-            with open(f"{path}/info.txt", 'w') as f:
-                f.write(f"# steps         = {steps}\n")
-                f.write(f"# queries       = {queries}\n")
-                f.write(f"# intermediate  = {len(intermediate)}\n")
-                f.write(f"# states        = {size}\n")
+                os.system(f"diff {infodir}/h{i - 1}.dot {infodir}/h{i}.dot > {infodir}/diff_{i - 1}_{i}.txt")
+            
+            size = info['automaton_size']
             states.append(size)
             oracle_status.append((name, size == correct))
-    
-        results[f'{file.stem}'] = oracle_status
-    
-        if not all([s == correct for s in states]):
-            print(f"correct is {correct} : {states}")
-    
+
+        results[file.stem] = oracle_status
+
     with open('results.pkl', 'wb') as f:
         pickle.dump(results, f)
 else:
