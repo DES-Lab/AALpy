7995598d6d31b701130a5eaf182c7ff90183e630
steve-anunknown <steve.anunknown@gmail.com>
2024-12-06 00:48:23 +0200

    better benchmarks and results analysis

--- mealy_conformance_testing.py
+++ mealy_conformance_testing.py
@@ -2,10 +2,12 @@ import os
 import pandas as pd
 import pickle
 import random
+import json
 from pathlib import Path
 from aalpy import oracles
 from aalpy.SULs.AutomataSUL import AutomatonSUL
 from aalpy.learning_algs.deterministic.LStar import run_Lstar
+from aalpy.learning_algs.deterministic.KV import run_KV
 from aalpy.utils.FileHandler import load_automaton_from_file
 
 resultsdir = "results"
@@ -22,87 +24,101 @@ attributes = ['learning_rounds',
               'characterization_set',
               'intermediate_hypotheses']
 
-if not os.path.exists(resultsdir):
+if os.path.exists(resultsdir):
+    with open(resultsdir + '/results.pkl', 'rb') as f:
+        results = pickle.load(f)
+    # do not print the characterisation set
+    print(json.dumps(results, indent=4))
+    exit()
 
-    if not os.path.exists(resultsdir):
-        os.makedirs(resultsdir)
+class StatePrefixEqOracleTrue(oracles.StatePrefixEqOracle):
+    def __init__(self, alphabet, sul):
+        super().__init__(alphabet, sul, walks_per_state=200, walk_len=50, depth_first=True)
 
-    root = os.getcwd() + "/DotModels"
-    protocols = ["MQTT", "TLS"]
-    directories = [Path(root + '/' + prot) for prot in protocols]
-    files = [file for dir in directories for file in dir.iterdir()]
-    models = [load_automaton_from_file(f, 'mealy') for f in files]
+class StatePrefixEqOracleFalse(oracles.StatePrefixEqOracle):
+    def __init__(self, alphabet, sul):
+        super().__init__(alphabet, sul, walks_per_state=200, walk_len=50, depth_first=False)
 
-    class StatePrefixEqOracleTrue(oracles.StatePrefixEqOracle):
-        def __init__(self, alphabet, sul):
-            super().__init__(alphabet, sul, walks_per_state=50, depth_first=True)
+os.makedirs(resultsdir)
 
-    class StatePrefixEqOracleFalse(oracles.StatePrefixEqOracle):
-        def __init__(self, alphabet, sul):
-            super().__init__(alphabet, sul, walks_per_state=50, depth_first=False)
+root = os.getcwd() + "/DotModels"
+# for mqtt and tls, walks_per_state=200, walk_len=50
+# for tcp, a whole lot more
+# protocols = ["MQTT", "TLS", "TCP"]
+protocols = ["MQTT", "TLS"]
+true = {key : 0 for key in protocols}
+total = {key : 0 for key in protocols}
+directories = [Path(root + '/' + prot) for prot in protocols]
+files = [file for dir in directories for file in dir.iterdir()]
+models = [load_automaton_from_file(f, 'mealy') for f in files]
 
-    results = {} 
-    for file, model in zip(files, models):
-        alphabet = model.get_input_alphabet()
-        sul = AutomatonSUL(model)
+eqos = [
+        # "KWayStateCoverageEqOracle",
+        # "CacheBasedEqOracle",
+        # "TransitionFocusOracle",
+        "RandomWMethodEqOracle",
+        "StatePrefixEqOracleFalse",
+        "StatePrefixEqOracleTrue",
+        ]
 
-        oracle_status = []
+# dictionary for experiment results
+results = {}
+for file, model in zip(files, models):
+    alphabet = model.get_input_alphabet()
+    sul = AutomatonSUL(model)
+    correct_size = model.size
+    
+    # initialize empty dict for specific model
+    results[file.stem] = {}
+    for index, eqo in enumerate(eqos):
+        # equivalence checking algorithm name
+        checking = eqo
+        # initialize empty dictionary
+        results[file.stem][checking] = {}
+        for learning in ["lstar", "kv"]:
+            total[file.parent.name] += 1
+            orcs = [
+                    # oracles.KWayStateCoverageEqOracle(alphabet, sul, random_walk_len=100, k=4, method='permutations'),
+                    # oracles.CacheBasedEqOracle(alphabet, sul, num_walks=10000),
+                    # oracles.TransitionFocusOracle(alphabet, sul, num_random_walks=10000, walk_len=100),
+                    oracles.RandomWMethodEqOracle(alphabet, sul, walks_per_state=200, walk_len=50),
+                    StatePrefixEqOracleFalse(alphabet, sul),
+                    StatePrefixEqOracleTrue(alphabet, sul),
+                    ]
+            orc = orcs[index]
+            assert orc.__class__.__name__ == eqo
 
-        # deterministic
-        orcs = [
-                StatePrefixEqOracleFalse(alphabet, sul),
-                StatePrefixEqOracleTrue(alphabet, sul),
-                oracles.KWayStateCoverageEqOracle(alphabet, sul, random_walk_len=40),
-                oracles.TransitionFocusOracle(alphabet, sul, walk_len=40),
-                oracles.CacheBasedEqOracle(alphabet, sul),
-                oracles.PacOracle(alphabet, sul),
-                oracles.RandomWMethodEqOracle(alphabet, sul, walks_per_state=50, walk_len=40)
-                ]
+            if learning == "lstar":
+                model, info = run_Lstar(alphabet, sul, orc, 'mealy', print_level=0, return_data=True)
+            else:
+                model, info = run_KV(alphabet, sul, orc, 'mealy', print_level=0, return_data=True)
 
-        correct = model.size
-        states = []
-        for eqo in orcs:
-
-            l_star_model, info = run_Lstar(alphabet, sul, eqo, 'mealy', print_level=0, return_data=True)
-            name = eqo.__class__.__name__
-            infodir = resultsdir + '/' + name + '/' + file.stem
+            # directory follows structure of dict
+            infodir = resultsdir + '/' + file.stem + '/' + checking + '/' + learning
             if not os.path.exists(infodir):
                 os.makedirs(infodir)
 
-            infofile = resultsdir + '/' + name + '/' + file.stem + '/info.pkl'
-            with open(infofile, 'wb') as f:
-                pickle.dump(info, f)
-                
+            # save intermediate hypotheses for inspection
             intermediate = info['intermediate_hypotheses']
             for number, hyp in enumerate(intermediate):
                 hyp.save(file_path=(infodir+ f"/h{number}.dot"))
 
-            # also save the diffs of consecutive hypotheses
-            # this can be done by executing diff on the command line
-            # diff hyp1.dot hyp2.dot > diff1_2.txt
+            # calculate diffs between hypotheses for inspection
             for i in range(1, len(intermediate)):
                 os.system(f"diff {infodir}/h{i - 1}.dot {infodir}/h{i}.dot > {infodir}/diff_{i - 1}_{i}.txt")
-            
-            size = info['automaton_size']
-            states.append(size)
-            oracle_status.append((name, size == correct))
 
-        results[file.stem] = oracle_status
+            # save experiment results expect for intermediate hypotheses
+            results[file.stem][checking][learning] = { k : v for k , v in info.items() 
+                                                        if not k == "intermediate_hypotheses" }
 
-    with open(resultsdir + '/results.pkl', 'wb') as f:
-        pickle.dump(results, f)
-else:
-    with open(resultsdir + '/results.pkl', 'rb') as f:
-        results = pickle.load(f)
+            if not info['automaton_size'] == correct_size:
+                print(f"{file.stem} {checking} {learning} => Wrong size {info['automaton_size']} / {correct_size}")
+            else:
+                true[file.parent.name] += 1
 
-randkey = random.choice(list(results.keys()))
-index = [ first for first, _ in results[randkey] ]
-new_results= { key: [second for _, second in value] for key, value in results.items() } 
-df = pd.DataFrame(new_results, index=index)
-print(df)
-df.to_pickle(resultsdir + '/results-pd.pkl')
+for key, value in total.items():
+    print(f"{key} : {true[key]} / {value}")
 
-for index, row in df.iterrows():
-    truths = row.value_counts()[True]
-    falses = len(row) - truths
-    print(f"{index} learned correctly {truths} times and failed {falses} times")
+with open(resultsdir + '/results.pkl', 'wb') as f:
+    pickle.dump(results, f)
+
--- mealy_conformance_testing_results.py
+++ mealy_conformance_testing_results.py
@@ -1,30 +1,39 @@
 import pickle
 import os
+import json
 from pathlib import Path
+import numpy as np
+import math
 import matplotlib.pyplot as plt
+import sys
 
-# the results folder
-resultsdir = "results"
+# read the results dir from first argument
+if len(sys.argv) > 1:
+    resultsdir = sys.argv[1]
+    if not os.path.exists(resultsdir):
+        print(f"No {resultsdir} directory. Run mealy_conformance_testing.py first.")
+        exit(1)
+else:
+    print("Please provide the results directory as an argument.")
+    exit(1)
 
-# the file names of learned models
-root = os.getcwd() + "/DotModels"
-protocols = ["MQTT", "TLS"]
-directories = [Path(root + '/' + prot) for prot in protocols]
-files = [file for dir in directories for file in dir.iterdir()]
+# get the names of the directories in the results folder
+files = [file for file in Path(resultsdir).iterdir() if file.is_dir()]
 
 # the equivalence oracles that were tested
-orcs = [
-        "StatePrefixEqOracleFalse",
-        "StatePrefixEqOracleTrue",
-        "KWayStateCoverageEqOracle",
-        "TransitionFocusOracle",
-        "CacheBasedEqOracle",
-        "PacOracle",
-        "RandomWMethodEqOracle"
-        ]
+# iterate over a file in the results directory
+# get the basename
+orcs = [equiv_oracle.name for equiv_oracle in files[0].iterdir() if equiv_oracle.is_dir()]
 
 # results dir for each oracle:
-# resultsdir + '/' + orc + '/' + file.stem 
+# resultsdir + '/' + file.stem + '/' + checking + '/' + learning
+
+if not os.path.exists(resultsdir):
+    print(f"No {resultsdir} directory. Run mealy_conformance_testing.py first.")
+
+with open(resultsdir + '/results.pkl', 'rb') as f:
+    # this is a pandas dataframe
+    results = pickle.load(f)
 
 # the attributes inside the info.pkl dictionary
 attributes = ['learning_rounds',
@@ -39,49 +48,47 @@ attributes = ['learning_rounds',
               'characterization_set',
               'intermediate_hypotheses']
 
-if not os.path.exists(resultsdir):
-    print(f"No {resultsdir} directory. Run mealy_conformance_testing.py first.")
+# the results have the following structure:
+# results[model][equivalence_oracle][learning_alg] = infos
+# where infos is a dict of the attributes above
 
-with open(resultsdir + '/results-pd.pkl', 'rb') as f:
-    # this is a pandas dataframe
-    results_df = pickle.load(f)
+# for every equivalence oracle and learning algorithm,
+# plot the number of equivalence and membership queries
+# across all models
 
-print(results_df)
-
-for index, row in results_df.iterrows():
-    truths = row.value_counts()[True]
-    falses = len(row) - truths
-    print(f"{index} learned correctly {truths} times and failed {falses} times")
-
+width = 0.4  # Width of a bar
+xaxis = np.arange(len(files))
 for orc in orcs:
-    sizes = []
-    queries_learning = []
-    queries_checking = []
-    for file in files:
-        infofile = resultsdir + '/' + orc + '/' + file.stem + '/info.pkl'
-        with open(infofile, 'rb') as f:
-            info = pickle.load(f)
+    for learning in ["lstar", "kv"]:
+        checking_queries = [results[model][orc][learning]['queries_eq_oracle'] for model in results]
+        learning_queries = [results[model][orc][learning]['queries_learning'] for model in results]
 
-        sizes.append(info['automaton_size'])
-        queries_learning.append(info['queries_learning'])
-        queries_checking.append(info['queries_eq_oracle'])
+        # plot the bars side by side
+        plt.bar(xaxis,         checking_queries, width, label="Checking queries")
+        plt.bar(xaxis + width, learning_queries, width, label="Learning queries")
+        plt.xticks(xaxis + width / 2, [f.stem.split('_')[0] for f in files])
+        plt.xlabel("Model")
+        plt.ylabel("Queries")
+        plt.title(f"Number of queries for {orc} and {learning} learning")
+        plt.xticks(rotation=20)
+        plt.legend()
+        plt.savefig(f"{resultsdir}/{orc}_{learning}_queries.png")
+        plt.clf()
 
-    # create a bar plot for the queries during learning
-    # and another for the queries during checking
-    fig, ax = plt.subplots()
-    ax.bar([f.stem for f in files], queries_learning)
-    ax.set_ylabel('Queries')
-    ax.set_title(f'Queries during learning for {orc}')
-    plt.xticks(rotation=30)
-    plt.tight_layout()
-    plt.show()
+for learning in ["lstar", "kv"]:
+    plt.xticks(xaxis, [f.stem.split('_')[0] for f in files])
+    plt.xlabel("Model")
+    plt.ylabel("Queries")
+    plt.xticks(rotation=20)
 
-    fig, ax = plt.subplots()
-    ax.bar([f.stem for f in files], queries_checking)
-    ax.set_ylabel('Queries')
-    ax.set_title(f'Queries during checking for {orc}')
-    plt.xticks(rotation=30)
-    plt.tight_layout()
-    plt.show()
+    index = 0
+    for orc in orcs:
+        checking_steps = [results[model][orc][learning]['steps_eq_oracle'] for model in results]
+        plt.bar(xaxis + index * width / 3, checking_steps, width, label=orc)
+        index += 1
+    plt.title(f"Number of queries for {learning} learning")
+    plt.legend()
+    plt.savefig(f"{resultsdir}/{learning}_steps_eq_oracle.png")
+    plt.clf()
 
 
