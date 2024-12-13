c0058a2ee8d078d2bf4751fda390fe6ce9c65fe6
steve-anunknown <steve.anunknown@gmail.com>
2024-12-13 13:19:09 +0200

    enhancement

--- mealy_conformance_testing.py
+++ mealy_conformance_testing.py
@@ -3,12 +3,18 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from aalpy.oracles import RandomWMethodEqOracle
+from aalpy.oracles import PerfectKnowledgeEqOracle
 from aalpy.oracles.SortedStateCoverageEqOracle import SortedStateCoverageEqOracle
 from aalpy.oracles.InterleavedStateCoverageEqOracle import InterleavedStateCoverageEqOracle
 from aalpy.oracles.StochasticStateCoverageEqOracle import StochasticStateCoverageEqOracle
 from aalpy.SULs.AutomataSUL import AutomatonSUL
 from aalpy.learning_algs.deterministic.LStar import run_Lstar
 from aalpy.utils.FileHandler import load_automaton_from_file
+
+# print up to 1 decimal point
+np.set_printoptions(precision=1)
+# do not print in scientific notation
+np.set_printoptions(suppress=True)
 
 # class NewFirst(SortedStateCoverageEqOracle):
 #     def __init__(self, alphabet, sul, walks_per_state=4000, walk_len=150, mode='newest'):
@@ -46,13 +52,23 @@ class StochasticExponential(StochasticStateCoverageEqOracle):
     def __init__(self, alphabet, sul, walks_per_round=200000, walk_len=150, prob_function='exponential'):
         super().__init__(alphabet, sul, walks_per_round, walk_len, prob_function)
 
+TEACHER = "RandomWMethodEqOracle"
+# TEACHER = "PerfectKnowledgeEqOracle"
 
-def learn_model(alphabet, sul, name):
-    directory = f"stefanos_test/{name}"
+if not os.path.exists(TEACHER):
+    os.makedirs(TEACHER)
+
+def learn_model(alphabet, sul, model, name):
+    directory = f"{TEACHER}/{name}"
     if not os.path.exists(directory):
         os.makedirs(directory)
-
-        oracle = RandomWMethodEqOracle(alphabet, sul, walks_per_state=10000, walk_len=150)
+        
+        if TEACHER == "PerfectKnowledgeEqOracle":
+            oracle = PerfectKnowledgeEqOracle(alphabet, sul, model)
+        elif TEACHER == "RandomWMethodEqOracle":
+            oracle = RandomWMethodEqOracle(alphabet, sul, walks_per_state=10000, walk_len=150)
+        else:
+            raise ValueError("Unknown teacher")
         _, learning_info = run_Lstar(alphabet, sul, oracle, 'mealy', return_data=True, print_level=0)
         intermediate_hypotheses = learning_info['intermediate_hypotheses']
         for num, hyp in enumerate(intermediate_hypotheses):
@@ -66,22 +82,24 @@ def learn_model(alphabet, sul, name):
 
 def test_oracles(oracles, hyps, name):
     queries = np.zeros((len(oracles), len(hyps)))
+    failures = np.zeros((len(oracles), len(hyps)))
     # counterexamples are immutable, so we can't store them in a numpy array
     cexs = [[None for _ in range(len(hyps))] for _ in range(len(oracles))]
     for i, hyp in enumerate(hyps):
-        for j, oracle in enumerate(oracles):
+        for j, oracle in enumerate(oracles): 
             cexs[j][i] = oracle.find_cex(hyp)
             queries[j, i] = oracle.num_queries
-            if cexs[j][i] is None:
-                print(f"Oracle {oracle.__class__.__name__} failed at h{i}")
-    return queries
+            failures[j][i] = 1 if cexs[j][i] is None else 0
+    return queries, failures
 
 
 ROOT = os.getcwd() + "/DotModels"
-PROTOCOLS = ["TLS", "MQTT", "TCP"]
+# PROTOCOLS = ["ASML", "TLS", "MQTT", "EMV", "TCP"]
+PROTOCOLS = ["TCP"]
 DIRS = [Path(ROOT + '/' + prot) for prot in PROTOCOLS]
 FILES = [file for dir in DIRS for file in dir.iterdir()]
 MODELS = [load_automaton_from_file(f, 'mealy') for f in FILES]
+MODELS.sort(key=lambda x: x.size)
 
 # We will learn every model using the RandomWpEqOracle and we will retain the
 # intermediate hypotheses of the learning experiment.
@@ -92,35 +110,37 @@ MODELS = [load_automaton_from_file(f, 'mealy') for f in FILES]
 
 TIMES = 20
 NUM_ORACLES = 7
+huge = []
+tiny = []
 for index, (model, file) in enumerate(zip(MODELS, FILES)):
-    print("=====================================================")
     name = file.stem
-
     alphabet = model.get_input_alphabet()
+    if model.size > 150:
+        huge.append((name, model.size, len(alphabet)))
+        continue
     sul = AutomatonSUL(model)
 
     # this has the side effect of saving the intermediate hypotheses
     # in the directory "stefanos_test/{name}"
-    hypotheses = learn_model(alphabet, sul, name)
-
+    hypotheses = learn_model(alphabet, sul, model, name)
     # indicator that the model has been learned correctly
     if not hypotheses[-1].size == model.size:
-        print(f"Model {name} was not learned successfully. Skipping ... ")
+        print(f"Model not learned successfully {hypotheses[-1].size} / {model.size}. Skipping ... ")
         continue
 
     if len(hypotheses) == 1:
-        print(f"Model {name} was learned in just 1 round, therefore no counterexamples are expected. Skipping ...")
+        tiny.append((name, model.size, len(alphabet)))
         continue
 
-    print(f"Number of {name} hypotheses: {len(hypotheses)} {[h.size for h in hypotheses]}")
-    print(f"Size of alphabet is {len(alphabet)}")
-
+    print(f"==================={file.stem}====================")
+    print(f"Number of hypotheses: {len(hypotheses)} {[h.size for h in hypotheses]}")
     intermediate = hypotheses[:-1]
 
     # We will now try to find counterexamples for every intermediate hypothesis
     # using both StatePrefixEqOracles.
     NUM_HYPS = len(intermediate)
     means = np.zeros((NUM_ORACLES, NUM_HYPS))
+    failures = np.zeros((NUM_ORACLES, NUM_HYPS))
     for trial in range(TIMES):
         oracle1 = Random(alphabet, sul)
         oracle2 = InterleavedRandom(alphabet, sul)
@@ -130,17 +150,22 @@ for index, (model, file) in enumerate(zip(MODELS, FILES)):
         oracle6 = StochasticSquare(alphabet, sul)
         oracle7 = StochasticExponential(alphabet, sul)
         oracles = [oracle1, oracle2, oracle3, oracle4, oracle5, oracle6, oracle7]
-        queries = test_oracles(oracles, intermediate, name)
+        queries, fails = test_oracles(oracles, intermediate, name)
         means += queries
+        failures += fails
 
     means /= TIMES
-
-    # print up to 1 decimal point
-    np.set_printoptions(precision=1)
-    # do not print in scientific notation
-    np.set_printoptions(suppress=True)
-    oracles = [oracle1, oracle2, oracle3, oracle4, oracle5, oracle6, oracle7]
+    failures /= TIMES
     # print as panda dataframe
     df = pd.DataFrame(means, columns=[f"h{i}" for i in range(NUM_HYPS)], index=[o.__class__.__name__ for o in oracles])
     print(df)
+    df.to_csv(f"{TEACHER}/{name}/queries.csv")
+    df = pd.DataFrame(failures, columns=[f"h{i}" for i in range(NUM_HYPS)], index=[o.__class__.__name__ for o in oracles])
+    print(df)
+    df.to_csv(f"{TEACHER}/{name}/failures.csv")
+
+print("These models are too big to learn:")
+print(huge)
+print("These models are too small to learn:")
+print(tiny)
 
