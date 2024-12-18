b4f4506d16a71e5c1307ea22cb62d4433a377d8f
steve-anunknown <steve.anunknown@gmail.com>
2024-12-18 11:08:22 +0200

    new measurements are stored and more easily inspected

--- mealy_conformance_testing.py
+++ mealy_conformance_testing.py
@@ -1,5 +1,5 @@
 import os
-import pathlib 
+import pathlib
 import pandas as pd
 import numpy as np
 from aalpy.oracles import RandomWMethodEqOracle
@@ -16,6 +16,8 @@ from aalpy.utils.FileHandler import load_automaton_from_file
 np.set_printoptions(precision=1)
 # do not print in scientific notation
 np.set_printoptions(suppress=True)
+# print up to 3 decimal point
+pd.options.display.float_format = '{:.3f}'.format
 
 # class NewFirst(SortedStateCoverageEqOracle):
 #     def __init__(self, alphabet, sul, walks_per_state=4000, walk_len=150, mode='newest'):
@@ -57,8 +59,8 @@ class StochasticExponential(StochasticStateCoverageEqOracle):
     def __init__(self, alphabet, sul, walks_per_round=200000, walk_len=150, prob_function='exponential'):
         super().__init__(alphabet, sul, walks_per_round, walk_len, prob_function)
 
-# TEACHER = "RandomWMethodEqOracle"
-TEACHER = "PerfectKnowledgeEqOracle"
+TEACHER = "RandomWMethodEqOracle"
+# TEACHER = "PerfectKnowledgeEqOracle"
 
 if not os.path.exists(TEACHER):
     os.makedirs(TEACHER)
@@ -67,7 +69,7 @@ def learn_model(alphabet, sul, model, name):
     directory = f"{TEACHER}/{name}"
     if not os.path.exists(directory):
         os.makedirs(directory)
-        
+
         if TEACHER == "PerfectKnowledgeEqOracle":
             oracle = PerfectKnowledgeEqOracle(alphabet, sul, model)
         elif TEACHER == "RandomWMethodEqOracle":
@@ -92,7 +94,7 @@ def test_oracles(oracles, hyps, name):
     # counterexamples are immutable, so we can't store them in a numpy array
     cexs = [[None for _ in range(len(hyps))] for _ in range(len(oracles))]
     for i, hyp in enumerate(hyps):
-        for j, oracle in enumerate(oracles): 
+        for j, oracle in enumerate(oracles):
             cexs[j][i] = oracle.find_cex(hyp)
             queries[j, i] = oracle.num_queries
             failures[j][i] = 1 if cexs[j][i] is None else 0
@@ -141,45 +143,53 @@ for index, (model, file) in enumerate(zip(MODELS, FILES)):
     print(f"==================={file.stem}====================")
     print(f"Number of hypotheses: {len(hypotheses)} {[h.size for h in hypotheses]}")
     intermediate = hypotheses[:-1]
-
-    # We will now try to find counterexamples for every intermediate hypothesis
-    # using both StatePrefixEqOracles.
     NUM_HYPS = len(intermediate)
-    measurements = np.zeros((TIMES, NUM_ORACLES, NUM_HYPS))
-    failures = np.zeros((NUM_ORACLES, NUM_HYPS))
-    for trial in range(TIMES):
-        oracle1 = Random(alphabet, sul)
-        oracle2 = InterleavedRandom(alphabet, sul)
-        oracle3 = InterleavedNewFirst(alphabet, sul)
-        oracle4 = InterleavedOldFirst(alphabet, sul)
-        oracle5 = StochasticLinear(alphabet, sul)
-        oracle6 = StochasticSquare(alphabet, sul)
-        oracle7 = StochasticExponential(alphabet, sul)
-        oracles = [oracle1, oracle2, oracle3, oracle4, oracle5, oracle6, oracle7]
-        queries, fails = test_oracles(oracles, intermediate, name)
-        measurements[trial] = queries
-        failures += fails
 
+    # check if the measurements file exists and load it if it does
+    flag = os.path.exists(f"{TEACHER}/{name}/measurements.npy") and os.path.exists(f"{TEACHER}/{name}/failures.csv")
+    if flag:
+        measurements = np.load(f"{TEACHER}/{name}/measurements.npy")
+        failures = pd.read_csv(f"{TEACHER}/{name}/failures.csv", index_col=0)
+        # the index of the dataframe holds the oracle names
+        index = failures.index.values
+        failures = failures.to_numpy()
+    else:
+        # We will now try to find counterexamples for every intermediate hypothesis
+        # using both StatePrefixEqOracles.
+        measurements = np.zeros((TIMES, NUM_ORACLES, NUM_HYPS))
+        failures = np.zeros((NUM_ORACLES, NUM_HYPS))
+        for trial in range(TIMES):
+            oracle1 = Random(alphabet, sul)
+            oracle2 = InterleavedRandom(alphabet, sul)
+            oracle3 = InterleavedNewFirst(alphabet, sul)
+            oracle4 = InterleavedOldFirst(alphabet, sul)
+            oracle5 = StochasticLinear(alphabet, sul)
+            oracle6 = StochasticSquare(alphabet, sul)
+            oracle7 = StochasticExponential(alphabet, sul)
+            oracles = [oracle1, oracle2, oracle3, oracle4, oracle5, oracle6, oracle7]
+            queries, fails = test_oracles(oracles, intermediate, name)
+            measurements[trial] = queries
+            failures += fails
+        
+        index = [o.__class__.__name__ for o in oracles]
+        # save all measurements so that they can be inspected later
+        np.save(f"{TEACHER}/{name}/measurements.npy", measurements)
     averages = np.mean(measurements, axis=0)
     deviations = np.std(measurements, axis=0, mean=averages)
+    # also save the geometric mean and the median
+    geometric_means = np.exp(np.mean(np.log(measurements), axis=0))
+    medians = np.median(measurements, axis=0)
+    columns = [f"h{i}" for i in range(NUM_HYPS)]
 
     failures /= TIMES
 
-    # print as panda dataframe
-    df = pd.DataFrame(averages, columns=[f"h{i}" for i in range(NUM_HYPS)], index=[o.__class__.__name__ for o in oracles])
-    df.index.name = 'oracle'
-    print(df)
-    df.to_csv(f"{TEACHER}/{name}/queries.csv")
-
-    df = pd.DataFrame(deviations, columns=[f"h{i}" for i in range(NUM_HYPS)], index=[o.__class__.__name__ for o in oracles])
-    df.index.name = 'oracle'
-    print(df)
-    df.to_csv(f"{TEACHER}/{name}/deviations.csv")
-
-    df = pd.DataFrame(failures, columns=[f"h{i}" for i in range(NUM_HYPS)], index=[o.__class__.__name__ for o in oracles])
-    df.index.name = 'oracle'
-    print(df)
-    df.to_csv(f"{TEACHER}/{name}/failures.csv")
+    for stat, stat_name in zip([averages, deviations, geometric_means, medians, failures],
+                          ['averages', 'deviations', 'geometric_means', 'medians', 'failures']):
+        df = pd.DataFrame(stat, columns=columns, index=index)
+        df.index.name = 'oracle'
+        print(df)
+        if not flag:
+            df.to_csv(f"{TEACHER}/{name}/{stat_name}.csv")
 
 print("These models are too big to learn:")
 print(huge)
