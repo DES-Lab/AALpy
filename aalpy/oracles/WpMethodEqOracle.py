8e418f99c522974a315c8275f81466c49b3700ef
steve-anunknown <steve.anunknown@gmail.com>
2025-01-14 21:34:23 +0200

    rewrote so that the test cases of each phase are generators or iterators

--- aalpy/oracles/WpMethodEqOracle.py
+++ aalpy/oracles/WpMethodEqOracle.py
@@ -1,6 +1,6 @@
 from aalpy.base.Oracle import Oracle
 from aalpy.base.SUL import SUL
-from itertools import product, chain
+from itertools import product, chain, tee
 
 
 def state_characterization_set(hypothesis, alphabet, state):
@@ -21,7 +21,39 @@ def state_characterization_set(hypothesis, alphabet, state):
     return result
 
 
+def i_star(alph, upto):
+    """
+    Return an iterator that generates all possible sequences of length upto from the given alphabet.
+    Args:
+        alph: input alphabet
+        upto: maximum length of the sequences
+    """
+    return chain(*(product(alph, repeat=i) for i in range(upto)))
+
+
+def second_phase_it(hyp, alph, difference, middle):
+    """
+    Return an iterator that generates all possible sequences for the second phase of the Wp-method.
+    Args:
+        hyp: hypothesis automaton
+        alph: input alphabet
+        difference: set of sequences that are in the transition cover but not in the state cover
+        middle: iterator that generates all possible sequences of length upto from the given alphabet
+    """
+    for t, mid in product(difference, middle):
+        _ = hyp.execute_sequence(hyp.initial_state, t + mid)
+        state = hyp.current_state
+        char_set = state_characterization_set(hyp, alph, state)
+        concatenated = product([t], [mid], char_set)
+        for el in concatenated:
+            yield el
+
+
 class WpMethodEqOracle(Oracle):
+    """
+    Implements the Wp-method equivalence oracle.
+    """
+
     def __init__(self, alphabet: list, sul: SUL, max_number_of_states):
         super().__init__(alphabet, sul)
         self.m = max_number_of_states
@@ -39,22 +71,15 @@ class WpMethodEqOracle(Oracle):
         state_cover = set(state.prefix for state in hypothesis.states)
         difference = transition_cover.difference(state_cover)
 
-        middle = []
-        for i in range(min(self.m + 1 - len(hypothesis.states), 3)):
-            middle.extend(list(product(self.alphabet, repeat=i)))
-        first_phase = product(state_cover, middle, hypothesis.characterization_set)
-
-        # the second_phase consists of the transition in the transition_cover
-        # that are not in the state_cover, concatenated with the corresponding
-        # characterization set of the state that the transition leads to.
-        second_phase = set()
-        for t, mid in product(difference, middle):
-            _ = hypothesis.execute_sequence(hypothesis.initial_state, t + mid)
-            state = hypothesis.current_state
-            char_set = state_characterization_set(hypothesis, self.alphabet, state)
-            concatenated = product([t], [mid], char_set)
-            second_phase.update(concatenated)
-
+        # not really helpful but it's here
+        minimum = min(self.m + 1 - len(hypothesis.states), 3)
+        # two views of the same iterator
+        middle_1, middle_2 = tee(i_star(self.alphabet, minimum), 2)
+        # first phase State Cover * Middle * Characterization Set
+        first_phase = product(state_cover, middle_1, hypothesis.characterization_set)
+        # second phase (Transition Cover - State Cover) * Middle * Characterization Set
+        # of the state that the prefix leads to
+        second_phase = second_phase_it(hypothesis, self.alphabet, difference, middle_2)
         test_suite = chain(first_phase, second_phase)
         for seq in test_suite:
             inp_seq = tuple([i for sub in seq for i in sub])
